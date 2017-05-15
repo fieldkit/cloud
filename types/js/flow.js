79fe16580e2b78ed08c1f71e5047d789663342be
Eric Buth <buth@users.noreply.github.com>
2017-05-15 17:49:17 -0400

    types

---
+++ types/js/flow.js
@@ -0,0 +1,69 @@
+// @flow
+
+type LocationType = {
+  latitude?: number,
+  longitude?: number,
+  altitude?: number,
+}
+
+export class Location {
+  latitude: number;
+  longitude: number;
+  altitude: number;
+  constructor(obj: LocationType){
+    this.latitude = obj.latitude ?
+      this.latitude = obj.latitude : 0
+    this.longitude = obj.longitude ?
+      this.longitude = obj.longitude : 0
+    this.altitude = obj.altitude ?
+      this.altitude = obj.altitude : 0
+  }
+}
+
+type WeatherStationType = {
+  temperature?: number,
+  humidity?: number,
+  pressure?: number,
+}
+
+export class WeatherStation {
+  temperature: number;
+  humidity: number;
+  pressure: number;
+  constructor(obj: WeatherStationType){
+    this.temperature = obj.temperature ?
+      this.temperature = obj.temperature : 0
+    this.humidity = obj.humidity ?
+      this.humidity = obj.humidity : 0
+    this.pressure = obj.pressure ?
+      this.pressure = obj.pressure : 0
+  }
+}
+
+type FieldkitType = {
+  timestamp?: number,
+  cell_voltage?: number,
+  state_of_charge?: number,
+  location?: fieldkit.LocationType,
+  weather_station?: fieldkit.WeatherStationType,
+}
+
+export class Fieldkit {
+  timestamp: number;
+  cell_voltage: number;
+  state_of_charge: number;
+  location: ?fieldkit.Location;
+  weather_station: ?fieldkit.WeatherStation;
+  constructor(obj: FieldkitType){
+    this.timestamp = obj.timestamp ?
+      this.timestamp = obj.timestamp : 0
+    this.cell_voltage = obj.cell_voltage ?
+      this.cell_voltage = obj.cell_voltage : 0
+    this.state_of_charge = obj.state_of_charge ?
+      this.state_of_charge = obj.state_of_charge : 0
+    this.location = obj.location ?
+      new fieldkit.Location(obj.location) : null
+    this.weather_station = obj.weather_station ?
+      new fieldkit.WeatherStation(obj.weather_station) : null
+  }
+}
