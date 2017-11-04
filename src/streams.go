ec4929dcaf043f373d2d5dab1c4364db3db9dcfd
Jacob Lewallen <jlewalle@gmail.com>
2017-11-04 16:08:34 -0700

    Streams keep track of location changes. Return latest.
    Eventually we'll ask for the location at a given time. Eventually we should be able to say that two streams share location information.

--- src/streams.go
+++ src/streams.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"sort"
 	"time"
 )
 
@@ -10,15 +11,35 @@ type Location struct {
 	Coordinates []float32
 }
 
+type LocationTimes []int64
+
+func (a LocationTimes) Len() int           { return len(a) }
+func (a LocationTimes) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a LocationTimes) Less(i, j int) bool { return a[i] < a[j] }
+
 type Stream struct {
-	Id             SchemaId
-	LocationByTime map[int64]*Location
+	Id                  SchemaId
+	LocationChangeTimes LocationTimes
+	LocationByTime      map[int64]*Location
 }
 
 // There is a chance that we've "moved" previous messages that have come in over
 // this stream. So this should eventually trigger a replaying of them.
 func (ms *Stream) SetLocation(t *time.Time, l *Location) {
 	ms.LocationByTime[t.Unix()] = l
+	ms.LocationChangeTimes = append(ms.LocationChangeTimes, t.Unix())
+	sort.Sort(ms.LocationChangeTimes)
+}
+
+func (ms *Stream) HasLocation() bool {
+	return false
+}
+
+func (ms *Stream) GetLocation() (l *Location) {
+	if len(ms.LocationChangeTimes) == 0 {
+		return nil
+	}
+	return ms.LocationByTime[ms.LocationChangeTimes[len(ms.LocationChangeTimes)-1]]
 }
 
 type StreamsRepository interface {
