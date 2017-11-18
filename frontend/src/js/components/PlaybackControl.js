3e734abd11ef69cd3b4cb75c17f16efd465fc97a
Jacob Lewallen <jlewalle@gmail.com>
2017-11-17 23:11:11 -0800

    Gorgeous playback controls.

--- frontend/src/js/components/PlaybackControl.js
+++ frontend/src/js/components/PlaybackControl.js
@@ -4,7 +4,7 @@ const containerStyle: React.CSSProperties = {
     position: 'absolute',
     zIndex: 10,
     display: 'flex',
-    flexDirection: 'column',
+    flexDirection: 'row',
     boxShadow: '0px 1px 4px rgba(0, 0, 0, .3)',
     border: '1px solid rgba(0, 0, 0, 0.1)'
 };
@@ -16,8 +16,8 @@ const buttonStyle = {
     cursor: 'pointer',
     fontWeight: "bold",
     border: 0,
-    height: 26,
-    width: 26,
+    height: 32,
+    width: 36,
     backgroundPosition: '0px 0px',
     backgroundSize: '26px 260px',
     outline: 0
@@ -25,15 +25,15 @@ const buttonStyle = {
 
 const buttonTopStyle = {
     borderTopLeftRadius: 2,
-    borderTopRightRadius: 2
+    borderBottomLeftRadius: 2
 };
 
 const buttonInnerStyle = {
-    borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
+    borderRight: '1px solid rgba(0, 0, 0, 0.1)',
 };
 
 const buttonBottomStyle = {
-    borderBottomLeftRadius: 2,
+    borderTopRightRadius: 2,
     borderBottomRightRadius: 2
 };
 
@@ -57,26 +57,28 @@ export default class PlaybackControl extends Component {
     render() {
         const { className, playback, onPlaybackChange } = this.props;
 
-        const position = { top: 200, left: 30, bottom: 'auto', right: 'auto' };
+        const position = { top: 100, right: 30, bottom: 'auto', left: 'auto' };
         const smallStyle = {
             ...buttonStyle,
-            ...{ width: '80px' }
+            ...{ width: '30px' }
         };
 
+        const buttonClasses = "button";
         const playOrPause = (playback === PlaybackModes.Pause ||
                              playback === PlaybackModes.Beginning ||
                              playback === PlaybackModes.End) ? PlaybackModes.Play : PlaybackModes.Pause;
+        const playOrPauseIcon = playOrPause == PlaybackModes.Play ? "fa fa-play" : "fa fa-pause";
 
         return (
             <div
                 className={className}
                 style={{ ...containerStyle, ...position }}>
 
-                <button type="button" style={{ ...smallStyle, ...buttonInnerStyle, ...buttonTopStyle  }} onClick={ onPlaybackChange.bind(this, PlaybackModes.Beginning) }>Beg</button>
-                <button type="button" style={{ ...smallStyle, ...buttonInnerStyle }} onClick={ onPlaybackChange.bind(this, PlaybackModes.Rewind) }>RW</button>
-                <button type="button" style={{ ...smallStyle, ...buttonInnerStyle }} onClick={ onPlaybackChange.bind(this, playOrPause) }>{ playOrPause.mode }</button>
-                <button type="button" style={{ ...smallStyle, ...buttonInnerStyle }} onClick={ onPlaybackChange.bind(this, PlaybackModes.Forward) }>FF</button>
-                <button type="button" style={{ ...smallStyle, ...buttonBottomStyle }} onClick={ onPlaybackChange.bind(this, PlaybackModes.End) }>End</button>
+                <button type="button" style={{ ...smallStyle, ...buttonInnerStyle, ...buttonTopStyle  }} onClick={ onPlaybackChange.bind(this, PlaybackModes.Beginning) }><i className="fa fa-backward" aria-hidden="true"></i></button>
+                <button type="button" style={{ ...smallStyle, ...buttonInnerStyle }} onClick={ onPlaybackChange.bind(this, PlaybackModes.Rewind) }><i className="fa fa-step-backward" aria-hidden="true"></i></button>
+                <button type="button" style={{ ...smallStyle, ...buttonInnerStyle }} onClick={ onPlaybackChange.bind(this, playOrPause) }><i className={playOrPauseIcon} aria-hidden="true"></i></button>
+                <button type="button" style={{ ...smallStyle, ...buttonInnerStyle }} onClick={ onPlaybackChange.bind(this, PlaybackModes.Forward) }><i className="fa fa-forward" aria-hidden="true"></i></button>
+                <button type="button" style={{ ...smallStyle, ...buttonBottomStyle }} onClick={ onPlaybackChange.bind(this, PlaybackModes.End) }><i className="fa fa-step-forward" aria-hidden="true"></i></button>
             </div>
         )
     }
