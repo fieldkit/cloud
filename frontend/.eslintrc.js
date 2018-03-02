a7a891346e1d5a223bef69c2e401ee101bac1f3f
Jacob Lewallen <jlewallen@gmail.com>
2018-03-02 11:02:56 -0800

    Fixing eslintrc configuration.

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "rules": {
+        "indent": ["error", 4],
+        "no-extra-parens": 0,
+        "react/jsx-uses-vars": 1,
+        "react/jsx-uses-react": 1,
+    },
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "modules": true,
+            "jsx": true,
+        }
+    },
+    "env": {
+        "node": false,
+        "browser": true,
+        "es6": true,
+    },
+    "parser": "babel-eslint",
+    "plugins": [
+        "eslint-plugin-react"
+    ]
+}
