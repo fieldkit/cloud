de4ceaa7aa3d59d7024443da1d204cf140342aa1
Jacob Lewallen <jlewallen@gmail.com>
2018-03-13 16:36:43 -0700

    Show active time range in the criteria button.

--- frontend/src/js/components/CriteriaPanel.js
+++ frontend/src/js/components/CriteriaPanel.js
@@ -1,14 +1,18 @@
 // @flow weak
 
+import _ from 'lodash';
+
 import React, { Component } from 'react';
 
 class Range {
     title: string
     range: number
+    active: bool
 
-    constructor(title, range) {
+    constructor(title, range, active) {
         this.title = title;
         this.range = range;
+        this.active = active || false;
     }
 }
 
@@ -23,20 +27,53 @@ type State = {
 export default class CriteriaPanel extends Component {
     props: Props
     state: State
+    ranges: Array<Array<Range>>
 
     constructor(props) {
         super(props)
+
         this.state = {
             open: false
         }
+
+        this.ranges = [
+            [
+                new Range('Last 2 days', -86400 * 2),
+                new Range('Last 7 days', -86400 * 7),
+                new Range('Last 30 days', -86400 * 30),
+                new Range('Yesterday', -86400),
+            ],
+            [
+                new Range('Last 5 minutes', -60 * 5),
+                new Range('Last 15 minutes', -60 * 15),
+                new Range('Last 30 minutes', -60 * 30),
+                new Range('Last hour', -60 * 60),
+                new Range('Last 3 hours', -60 * 60 * 3, true),
+                new Range('Last 6 hours', -60 * 60 * 6),
+                new Range('Last 12 hours', -60 * 60 * 12),
+                new Range('Last 24 hours', -60 * 60 * 24),
+            ]
+        ];
+    }
+
+    componentWillMount() {
+        this.onChangeTimeCiteria(this.getActiveRange());
+    }
+
+    getActiveRange() {
+        return _(this.ranges).flatten().filter(r => r.active).first();
     }
 
     onChangeTimeCiteria(newRange) {
         const { onChangeTimeCiteria } = this.props;
 
+        const oldActive = this.getActiveRange()
+        oldActive.active = false;
+        newRange.active = true;
+
         const now = new Date();
         const start = new Date();
-        start.setTime(now.getTime() + (newRange * 1000));
+        start.setTime(now.getTime() + (newRange.range * 1000));
 
         onChangeTimeCiteria({
             startTime: Math.trunc(start.getTime() / 1000),
@@ -55,29 +92,10 @@ export default class CriteriaPanel extends Component {
     }
 
     renderOpen() {
-        const ranges = [
-            [
-                new Range('Last 2 days', -86400 * 2),
-                new Range('Last 7 days', -86400 * 7),
-                new Range('Last 30 days', -86400 * 30),
-                new Range('Yesterday', -86400),
-            ],
-            [
-                new Range('Last 5 minutes', -60 * 5),
-                new Range('Last 15 minutes', -60 * 15),
-                new Range('Last 30 minutes', -60 * 30),
-                new Range('Last hour', -60 * 60),
-                new Range('Last 3 hours', -60 * 60 * 3),
-                new Range('Last 6 hours', -60 * 60 * 6),
-                new Range('Last 12 hours', -60 * 60 * 12),
-                new Range('Last 24 hours', -60 * 60 * 24),
-            ]
-        ];
-
         return <div className="drawer">
-            {ranges.map((column, j) => <ul key={j}>
+            {this.ranges.map((column, j) => <ul key={j}>
                     {column.map((r, i) => {
-                        return <li key={i} onClick={ this.onChangeTimeCiteria.bind(this, r.range) }>{r.title}</li>;
+                        return <li key={i} onClick={ this.onChangeTimeCiteria.bind(this, r) }>{r.title}</li>;
                     })}
                 </ul>
             )}
@@ -85,8 +103,9 @@ export default class CriteriaPanel extends Component {
     }
 
     render() {
+        const active = this.getActiveRange();
         return <div className="criteria-panel">
-            <div onClick={this.onOpenToggle.bind(this)} className="button">Critera</div>
+            <div onClick={this.onOpenToggle.bind(this)} className="button">{active.title}</div>
             {this.state.open && this.renderOpen()}
         </div>;
     }
