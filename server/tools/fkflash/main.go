b07c57ed82fe7bc3fe24994a882151b81297be08
Jacob Lewallen <jlewallen@gmail.com>
2018-11-15 10:01:42 -0800

    Split some files up.

--- server/tools/fkflash/main.go
+++ server/tools/fkflash/main.go
@@ -2,23 +2,14 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
 	"path"
-	"sort"
-	"strings"
-	"time"
 
 	tooling "github.com/Conservify/tooling"
-	fk "github.com/fieldkit/cloud/server/api/client"
-	fktesting "github.com/fieldkit/cloud/server/tools"
 )
-
-import "text/tabwriter"
 
 type options struct {
 	Scheme         string
@@ -27,22 +18,12 @@ type options struct {
 	ListLocal      bool
 	ListPorts      bool
 	Download       bool
+	Offline        bool
 	All            bool
 	Module         string
 	CacheDirectory string
 	FirmwareID     int
 	Port           string
-}
-
-func getDefaultCacheDirectory() string {
-	home := os.Getenv("HOME")
-	if home == "" {
-		panic("Unable to find HOME")
-	}
-	return path.Join(home, ".fk/firmware")
-}
-
-func download() {
 }
 
 func main() {
@@ -54,13 +35,16 @@ func main() {
 
 	flag.StringVar(&o.Scheme, "scheme", "https", "fk instance scheme")
 	flag.StringVar(&o.Host, "host", "api.fkdev.org", "fk instance hostname")
+
 	flag.StringVar(&o.Module, "module", "", "module")
+	flag.BoolVar(&o.All, "all", false, "disable filtering by latest firmware")
+
+	flag.BoolVar(&o.Offline, "offline", false, "work in offline mode")
 
 	flag.BoolVar(&o.ListRemote, "remote", false, "list firmware")
 	flag.BoolVar(&o.ListLocal, "local", false, "list firmware")
 	flag.BoolVar(&o.ListPorts, "ports", false, "list ports")
 
-	flag.BoolVar(&o.All, "all", false, "disable filtering by latest firmware")
 	flag.BoolVar(&o.Download, "download", false, "download firmware and cache locally")
 
 	flag.IntVar(&o.FirmwareID, "flash", 0, "flash firmware to a connected device")
@@ -72,6 +56,9 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error: %v", err)
 		return
+	}
+
+	if !o.Offline {
 	}
 
 	if o.ListRemote {
@@ -171,212 +158,10 @@ func main() {
 	}
 }
 
-type FirmwareManager struct {
-	Directory string
-}
-
-func NewFirmwareManager(ctx context.Context, directory string) (fm *FirmwareManager, err error) {
-	fm = &FirmwareManager{
-		Directory: directory,
+func getDefaultCacheDirectory() string {
+	home := os.Getenv("HOME")
+	if home == "" {
+		panic("Unable to find HOME")
 	}
-
-	err = fm.Initialize(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	return
-}
-
-func (fm *FirmwareManager) Initialize(ctx context.Context) error {
-	return mkdirAllIfMissing(fm.Directory)
-}
-
-func (fm *FirmwareManager) ListLocal(ctx context.Context) (fc *FirmwareCollection, err error) {
-	files, err := ioutil.ReadDir(fm.Directory)
-	if err != nil {
-		return nil, err
-	}
-
-	fc = NewFirmwareCollection()
-
-	for _, file := range files {
-		if strings.HasSuffix(file.Name(), ".bin.json") {
-			path := path.Join(fm.Directory, file.Name())
-
-			bytes, err := ioutil.ReadFile(path)
-			if err != nil {
-				return nil, err
-			}
-
-			firmware := &Firmware{}
-			err = json.Unmarshal(bytes, &firmware)
-			if err != nil {
-				return nil, err
-			}
-
-			fc.Add(firmware)
-		}
-	}
-
-	return
-}
-
-func (fm *FirmwareManager) ListRemote(ctx context.Context, host, scheme string) (fc *FirmwareCollection, err error) {
-	c, err := fktesting.NewClient(ctx, host, scheme)
-	if err != nil {
-		return nil, err
-	}
-
-	res, err := c.ListFirmware(ctx, fk.ListFirmwarePath(), nil, nil)
-	if err != nil {
-		return nil, err
-	}
-	firmwares, err := c.DecodeFirmwares(res)
-	if err != nil {
-		return nil, err
-	}
-
-	fc = NewFirmwareCollection()
-
-	for _, f := range firmwares.Firmwares {
-		fc.Add(&Firmware{
-			ID:      f.ID,
-			Module:  f.Module,
-			Profile: f.Profile,
-			Time:    f.Time,
-			Etag:    f.Etag,
-			URL:     f.URL,
-			Path:    fmt.Sprintf("%s/%s.bin", fm.Directory, f.Etag),
-		})
-	}
-
-	return fc, nil
-}
-
-func (fm *FirmwareManager) Find(ctx context.Context, id int) (f *Firmware, err error) {
-	local, err := fm.ListLocal(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	for _, f := range local.Firmwares {
-		if f.ID == id {
-			return f, nil
-		}
-	}
-
-	return nil, fmt.Errorf("No such firmware with ID: %v", id)
-}
-
-func (fm *FirmwareManager) Download(fc *FirmwareCollection) error {
-	if fc.Empty() {
-		return nil
-	}
-	return DownloadAll(fc)
-}
-
-func mkdirAllIfMissing(p string) error {
-	if _, err := os.Stat(p); os.IsNotExist(err) {
-		err = os.MkdirAll(p, 0755)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-type FirmwareCollection struct {
-	Firmwares []*Firmware
-}
-
-type Firmware struct {
-	ID       int
-	Module   string
-	Profile  string
-	Time     time.Time
-	Etag     string
-	URL      string
-	Filename string
-	Path     string
-}
-
-type BySort []*Firmware
-
-func (a BySort) Len() int           { return len(a) }
-func (a BySort) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a BySort) Less(i, j int) bool { return a[i].Time.Unix() > a[j].Time.Unix() }
-
-func NewFirmwareCollection() (fc *FirmwareCollection) {
-	return &FirmwareCollection{
-		Firmwares: make([]*Firmware, 0),
-	}
-}
-
-func (fc *FirmwareCollection) NotLocal() *FirmwareCollection {
-	newFc := NewFirmwareCollection()
-
-	for _, f := range fc.Firmwares {
-		if f.Path != "" {
-			if !f.IsLocal() {
-				newFc.Add(f)
-			}
-		}
-	}
-
-	return newFc
-}
-
-func (fc *FirmwareCollection) Latest() *FirmwareCollection {
-	newFc := NewFirmwareCollection()
-
-	seen := make(map[string]bool)
-
-	sort.Sort(BySort(fc.Firmwares))
-
-	for _, f := range fc.Firmwares {
-		key := f.Module + "-" + f.Profile
-		if _, ok := seen[key]; !ok {
-			newFc.Firmwares = append(newFc.Firmwares, f)
-			seen[key] = true
-		}
-	}
-
-	return newFc
-}
-
-func (fc *FirmwareCollection) Empty() bool {
-	return len(fc.Firmwares) == 0
-}
-
-func (fc *FirmwareCollection) Add(f *Firmware) {
-	fc.Firmwares = append(fc.Firmwares, f)
-}
-
-func (fc *FirmwareCollection) Display() {
-	if len(fc.Firmwares) == 0 {
-		return
-	}
-
-	sort.Sort(BySort(fc.Firmwares))
-
-	w := tabwriter.NewWriter(os.Stdout, 8, 0, 4, ' ', tabwriter.Debug)
-	fmt.Fprintln(w, fmt.Sprintf("ID\t Module\t Profile\t Time\t Tag\t Path"))
-
-	for _, f := range fc.Firmwares {
-		if f.IsLocal() {
-			fmt.Fprintln(w, fmt.Sprintf("%d\t %s\t %s\t %s\t %s\t %s", f.ID, f.Module, f.Profile, f.Time, f.Etag, f.Path))
-		} else {
-			fmt.Fprintln(w, fmt.Sprintf("%d\t %s\t %s\t %s\t %s", f.ID, f.Module, f.Profile, f.Time, f.Etag))
-		}
-	}
-
-	w.Flush()
-}
-
-func (f *Firmware) IsLocal() bool {
-	if _, err := os.Stat(f.Path); os.IsNotExist(err) {
-		return false
-	}
-	return true
+	return path.Join(home, ".fk/firmware")
 }
