f56b57df4fa9f33d8adde7be3679b599eb3e23df
Jacob Lewallen <jlewallen@gmail.com>
2018-11-14 16:55:45 -0800

    Flashing support using tooling library.

--- server/tools/fkflash/main.go
+++ server/tools/fkflash/main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"path"
 	"sort"
+	"strings"
 	"time"
 
+	tooling "github.com/Conservify/tooling"
 	fk "github.com/fieldkit/cloud/server/api/client"
 	fktesting "github.com/fieldkit/cloud/server/tools"
 )
@@ -18,15 +23,26 @@ import "text/tabwriter"
 type options struct {
 	Scheme         string
 	Host           string
-	List           bool
+	ListRemote     bool
+	ListLocal      bool
+	ListPorts      bool
 	Download       bool
 	All            bool
 	Module         string
 	CacheDirectory string
+	FirmwareID     int
+	Port           string
 }
 
 func getDefaultCacheDirectory() string {
-	return ".fk/firmware"
+	home := os.Getenv("HOME")
+	if home == "" {
+		panic("Unable to find HOME")
+	}
+	return path.Join(home, ".fk/firmware")
+}
+
+func download() {
 }
 
 func main() {
@@ -38,11 +54,17 @@ func main() {
 
 	flag.StringVar(&o.Scheme, "scheme", "https", "fk instance scheme")
 	flag.StringVar(&o.Host, "host", "api.fkdev.org", "fk instance hostname")
-	flag.StringVar(&o.Module, "module", "", "override module")
+	flag.StringVar(&o.Module, "module", "", "module")
 
-	flag.BoolVar(&o.List, "list", false, "list firmware")
+	flag.BoolVar(&o.ListRemote, "remote", false, "list firmware")
+	flag.BoolVar(&o.ListLocal, "local", false, "list firmware")
+	flag.BoolVar(&o.ListPorts, "ports", false, "list ports")
+
 	flag.BoolVar(&o.All, "all", false, "disable filtering by latest firmware")
 	flag.BoolVar(&o.Download, "download", false, "download firmware and cache locally")
+
+	flag.IntVar(&o.FirmwareID, "flash", 0, "flash firmware to a connected device")
+	flag.StringVar(&o.Port, "port", "", "port to focus on when flashing")
 
 	flag.Parse()
 
@@ -52,8 +74,8 @@ func main() {
 		return
 	}
 
-	if o.List || o.Download {
-		fc, err := firmware.List(ctx, o.Host, o.Scheme)
+	if o.ListRemote {
+		fc, err := firmware.ListRemote(ctx, o.Host, o.Scheme)
 		if err != nil {
 			log.Fatalf("Error: %v", err)
 			return
@@ -71,7 +93,81 @@ func main() {
 			}
 		}
 
+		fmt.Println("REMOTE:")
+
 		fc.Display()
+	}
+
+	if o.Download {
+		fc, err := firmware.ListRemote(ctx, o.Host, o.Scheme)
+		if err != nil {
+			log.Fatalf("Error: %v", err)
+			return
+		}
+
+		if !o.All {
+			fc = fc.Latest()
+		}
+
+		err = firmware.Download(fc.NotLocal())
+		if err != nil {
+			log.Fatalf("Error: %v", err)
+			return
+		}
+	}
+
+	if o.ListLocal {
+		fc, err := firmware.ListLocal(ctx)
+		if err != nil {
+			log.Fatalf("Error: %v", err)
+			return
+		}
+
+		if fc.Empty() {
+			log.Printf("No local firmware. Try running with --download if you're online.")
+			return
+		}
+
+		if !o.All {
+			fc = fc.Latest()
+		}
+
+		fmt.Println("LOCAL:")
+
+		fc.Display()
+	}
+
+	if o.ListPorts {
+		pd := tooling.NewPortDiscoveror()
+		for _, p := range pd.List() {
+			fmt.Println("Port:", p.Name)
+		}
+	}
+
+	if o.FirmwareID > 0 {
+		f, err := firmware.Find(ctx, o.FirmwareID)
+		if err != nil {
+			log.Fatalf("Error: %v", err)
+		}
+
+		ae := tooling.NewArduinoEnvironment()
+
+		err = ae.Locate("")
+		if err != nil {
+			log.Fatalf("Error: %v", err)
+		}
+
+		tooling.Upload(&tooling.UploadOptions{
+			Arduino:     ae,
+			SkipTouch:   false,
+			Board:       "adafruit_feather_m0",
+			Binary:      f.Path,
+			Port:        "",
+			FlashOffset: 32768,
+			Verbose:     false,
+			Verify:      true,
+			Quietly:     false,
+		})
 	}
 }
 
@@ -96,7 +192,37 @@ func (fm *FirmwareManager) Initialize(ctx context.Context) error {
 	return mkdirAllIfMissing(fm.Directory)
 }
 
-func (fm *FirmwareManager) List(ctx context.Context, host, scheme string) (fc *FirmwareCollection, err error) {
+func (fm *FirmwareManager) ListLocal(ctx context.Context) (fc *FirmwareCollection, err error) {
+	files, err := ioutil.ReadDir(fm.Directory)
+	if err != nil {
+		return nil, err
+	}
+
+	fc = NewFirmwareCollection()
+
+	for _, file := range files {
+		if strings.HasSuffix(file.Name(), ".bin.json") {
+			path := path.Join(fm.Directory, file.Name())
+
+			bytes, err := ioutil.ReadFile(path)
+			if err != nil {
+				return nil, err
+			}
+
+			firmware := &Firmware{}
+			err = json.Unmarshal(bytes, &firmware)
+			if err != nil {
+				return nil, err
+			}
+
+			fc.Add(firmware)
+		}
+	}
+
+	return
+}
+
+func (fm *FirmwareManager) ListRemote(ctx context.Context, host, scheme string) (fc *FirmwareCollection, err error) {
 	c, err := fktesting.NewClient(ctx, host, scheme)
 	if err != nil {
 		return nil, err
@@ -126,6 +252,21 @@ func (fm *FirmwareManager) List(ctx context.Context, host, scheme string) (fc *F
 	}
 
 	return fc, nil
+}
+
+func (fm *FirmwareManager) Find(ctx context.Context, id int) (f *Firmware, err error) {
+	local, err := fm.ListLocal(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, f := range local.Firmwares {
+		if f.ID == id {
+			return f, nil
+		}
+	}
+
+	return nil, fmt.Errorf("No such firmware with ID: %v", id)
 }
 
 func (fm *FirmwareManager) Download(fc *FirmwareCollection) error {
@@ -213,8 +354,14 @@ func (fc *FirmwareCollection) Add(f *Firmware) {
 }
 
 func (fc *FirmwareCollection) Display() {
+	if len(fc.Firmwares) == 0 {
+		return
+	}
+
+	sort.Sort(BySort(fc.Firmwares))
+
 	w := tabwriter.NewWriter(os.Stdout, 8, 0, 4, ' ', tabwriter.Debug)
-	fmt.Fprintln(w, fmt.Sprintf("ID\t Module\t Profile\t Time\t Tag"))
+	fmt.Fprintln(w, fmt.Sprintf("ID\t Module\t Profile\t Time\t Tag\t Path"))
 
 	for _, f := range fc.Firmwares {
 		if f.IsLocal() {
