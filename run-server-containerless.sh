4c1dd12f673cb1ddeea3acc1fe993cfba9a78111
Jacob Lewallen <jlewallen@gmail.com>
2019-03-26 12:13:11 -0700

    Pull AWS config in.

--- run-server-containerless.sh
+++ run-server-containerless.sh
@@ -7,6 +7,10 @@ export FIELDKIT_POSTGRES_URL=postgres://fieldkit:password@127.0.0.1/fieldkit?ssl
 export FIELDKIT_DISABLE_MEMORY_LOGGING=true
 export FIELDKIT_DISABLE_STARTUP_REFRESH=true
 
+if [ -f aws.env ]; then
+    source aws.env
+fi
+
 make build/server
 
 build/server "$@"
