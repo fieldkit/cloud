92c08fc4252cfc5fd00f32762e6cd4b66d22041c
Jacob Lewallen <jlewallen@gmail.com>
2019-03-26 14:40:47 -0700

    Set dev mode API configuration.

--- run-server-containerless.sh
+++ run-server-containerless.sh
@@ -6,6 +6,8 @@ export FIELDKIT_ADDR=:8080
 export FIELDKIT_POSTGRES_URL=postgres://fieldkit:password@127.0.0.1/fieldkit?sslmode=disable
 export FIELDKIT_DISABLE_MEMORY_LOGGING=true
 export FIELDKIT_DISABLE_STARTUP_REFRESH=true
+export FIELDKIT_API_DOMAIN=127.0.0.1:8080
+export FIELDKIT_API_HOST=http://127.0.0.1:8080
 
 if [ -f aws.env ]; then
     source aws.env
