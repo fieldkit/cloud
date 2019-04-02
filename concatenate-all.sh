5c5401bc924159cc4b487116e30c235e4208b0e3
Jacob Lewallen <jlewallen@gmail.com>
2019-04-02 11:04:02 -0700

    Refresh logs/data in script.

--- concatenate-all.sh
+++ concatenate-all.sh
@@ -2,8 +2,17 @@
 
 ROOT=http://127.0.0.1:8080
 ROOT=https://api.fkdev.org
+DEVICES_JSON=/tmp/devices.json
 
-for url in `curl -s $ROOT/files/devices | jq -r ".devices[].urls.data.generate" | head -n 20`; do
+curl -s $ROOT/files/devices > $DEVICES_JSON
+
+for url in `cat $DEVICES_JSON | jq -r ".devices[].urls.data.generate" | head -n 20`; do
+    echo $url
+    curl -v $url
+done
+
+
+for url in `cat $DEVICES_JSON | jq -r ".devices[].urls.logs.generate" | head -n 20`; do
     echo $url
     curl -v $url
 done
