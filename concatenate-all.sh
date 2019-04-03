137716fee0fa2879753b24dff9a0d83489e8218b
Jacob Lewallen <jlewallen@gmail.com>
2019-04-03 11:09:32 -0700

    Concat script doesn't run up the queue.

--- concatenate-all.sh
+++ concatenate-all.sh
@@ -2,17 +2,33 @@
 
 ROOT=http://127.0.0.1:8080
 ROOT=https://api.fkdev.org
-DEVICES_JSON=/tmp/devices.json
 
-curl -s $ROOT/files/devices > $DEVICES_JSON
+DEVICES_JSON=$(curl -s $ROOT/files/devices)
 
-for url in `cat $DEVICES_JSON | jq -r ".devices[].urls.data.generate" | head -n 2000`; do
-    echo $url
-    curl -v $url
+function pause_for_empty_queue() {
+    while true; do
+        queue_length=$(curl -s $ROOT/files/status | jq '.queued')
+
+        echo "Queue:" $queue_length
+
+        if [ $queue_length -lt 1 ]; then
+            return
+        fi
+
+        echo "Waiting for empty queue..."
+
+        sleep 10
+    done
+}
+
+for device in `echo $DEVICES_JSON | jq -c '.devices | sort_by(.number_of_files) | .[] | { files: .number_of_files, data: .urls.data.generate, logs: .urls.logs.generate }'`; do
+    data=$(echo $device | jq -r '.data')
+    logs=$(echo $device | jq -r '.logs')
+
+    pause_for_empty_queue
+
+    echo $device
+    curl $data
+    curl $logs
 done
 
-
-for url in `cat $DEVICES_JSON | jq -r ".devices[].urls.logs.generate" | head -n 2000`; do
-    echo $url
-    curl -v $url
-done
