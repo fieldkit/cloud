c7c44eafbb6d0ca35a71aaf684c459053c5af99f
Jacob Lewallen <jlewallen@gmail.com>
2019-08-08 15:04:31 -0700

    Smarter local db script that checks for psql.

--- database-local.sh
+++ database-local.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-psql -h 127.0.0.1 -U fieldkit -U fieldkit
+if which psql; then
+	psql -h 127.0.0.1 -U fieldkit -U fieldkit
+else
+	# docker run -it --rm postgres psql -h 127.0.0.1 -U fieldkit -U fieldkit
+	docker exec -it cloud_postgres_1 psql -U fieldkit fieldkit
+fi
