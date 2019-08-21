c675c5e46b690111a6d92a6e4bc9d5fc0d69ad1f
Jacob Lewallen <jlewallen@gmail.com>
2019-08-21 12:44:00 -0700

    Cosmetic fixes and some tweaks to the latest merge.

--- server/api/station.go
+++ server/api/station.go
@@ -1,119 +1,115 @@
 package api
 
 import (
-  "fmt"
+	"fmt"
 
-  jwtgo "github.com/dgrijalva/jwt-go"
-  "github.com/goadesign/goa"
-  "github.com/goadesign/goa/middleware/security/jwt"
+	jwtgo "github.com/dgrijalva/jwt-go"
 
-  "github.com/conservify/sqlxcache"
+	"github.com/goadesign/goa"
+	"github.com/goadesign/goa/middleware/security/jwt"
 
-  "github.com/fieldkit/cloud/server/api/app"
-  "github.com/fieldkit/cloud/server/data"
+	"github.com/conservify/sqlxcache"
+
+	"github.com/fieldkit/cloud/server/api/app"
+	"github.com/fieldkit/cloud/server/data"
 )
 
 type StationControllerOptions struct {
-  Database *sqlxcache.DB
+	Database *sqlxcache.DB
 }
 
 func StationType(station *data.Station) *app.Station {
-  return &app.Station{
-    ID:         int(station.ID),
-    Name:       station.Name,
-    UserID:     int(station.UserID),
-  }
+	return &app.Station{
+		ID:     int(station.ID),
+		Name:   station.Name,
+		UserID: int(station.UserID),
+	}
 }
 
 func StationsType(stations []*data.Station) *app.Stations {
-  stationsCollection := make([]*app.Station, len(stations))
-  for i, station := range stations {
-    stationsCollection[i] = StationType(station)
-  }
+	stationsCollection := make([]*app.Station, len(stations))
 
-  return &app.Stations{
-    Stations: stationsCollection,
-  }
+	for i, station := range stations {
+		stationsCollection[i] = StationType(station)
+	}
+
+	return &app.Stations{
+		Stations: stationsCollection,
+	}
 }
 
-//StationController
 type StationController struct {
-  *goa.Controller
-  options StationControllerOptions
+	*goa.Controller
+	options StationControllerOptions
 }
 
-func NewStationController(service *goa.Service, options StationControllerOptions) * StationController {
-  return &StationController{
-    Controller: service.NewController("StationController"),
-    options:    options,
-  }
+func NewStationController(service *goa.Service, options StationControllerOptions) *StationController {
+	return &StationController{
+		Controller: service.NewController("StationController"),
+		options:    options,
+	}
 }
 
-func (c *StationController) Add(ctx *app.AddStationContext) error{
-  token := jwt.ContextJWT(ctx)
-  if token == nil {
-    return fmt.Errorf("JWT token is missing from context")
-  }
+func (c *StationController) Add(ctx *app.AddStationContext) error {
+	token := jwt.ContextJWT(ctx)
+	if token == nil {
+		return fmt.Errorf("JWT token is missing from context")
+	}
 
-  claims, ok := token.Claims.(jwtgo.MapClaims)
-  if !ok {
-    return fmt.Errorf("JWT claims error")
-  }
+	claims, ok := token.Claims.(jwtgo.MapClaims)
+	if !ok {
+		return fmt.Errorf("JWT claims error")
+	}
 
-  station := &data.Station{
-    Name:    ctx.Payload.Name,
-    UserID:  int32(ctx.Payload.UserID),
-  }
+	station := &data.Station{
+		Name:   ctx.Payload.Name,
+		UserID: claims["sub"].(int32), // NOTE Untested.
+	}
 
-  if err := c.options.Database.NamedGetContext(ctx, station, "INSERT INTO fieldkit.station (name, user_id) VALUES (:name, :user_id) RETURNING *", station); err != nil {
-    return err
-  }
-
-  if _, err := c.options.Database.ExecContext(ctx, "INSERT INTO fieldkit.station_user (station_id, user_id) VALUES ($1, $2)", station.ID, claims["sub"]); err != nil {
-    return err
-  }
-
-  return ctx.OK(StationType(station))
-}
-
-func (c *StationController) Update(ctx *app.UpdateStationContext) error {
-  station := &data.Station{
-    ID:         int32(ctx.StationID),
-    Name:       ctx.Payload.Name,
-  }
-
-  if err := c.options.Database.NamedGetContext(ctx, station, "UPDATE fieldkit.station SET name = :name, WHERE id = :id RETURNING *", station); err != nil {
+	if err := c.options.Database.NamedGetContext(ctx, station, "INSERT INTO fieldkit.station (name, user_id) VALUES (:name, :user_id) RETURNING *", station); err != nil {
 		return err
 	}
 
 	return ctx.OK(StationType(station))
 }
 
-//what does $1 mean?
-func (c *StationController) Get(ctx *app.GetStationContext) error {
-  station := &data.Station{}
-  if err := c.options.Database.GetContext(ctx, station, "SELECT * FROM fieldkit.station WHERE name = $1", ctx.Station); err != nil {
-    return err
-  }
+func (c *StationController) Update(ctx *app.UpdateStationContext) error {
+	station := &data.Station{
+		ID:   int32(ctx.StationID),
+		Name: ctx.Payload.Name,
+	}
 
-  return ctx.OK(StationType(station))
+	if err := c.options.Database.NamedGetContext(ctx, station, "UPDATE fieldkit.station SET name = :name, WHERE id = :id RETURNING *", station); err != nil {
+		return err
+	}
+
+	return ctx.OK(StationType(station))
+}
+
+func (c *StationController) Get(ctx *app.GetStationContext) error {
+	station := &data.Station{}
+	if err := c.options.Database.GetContext(ctx, station, "SELECT * FROM fieldkit.station WHERE name = $1", ctx.Station); err != nil {
+		return err
+	}
+
+	return ctx.OK(StationType(station))
 }
 
 func (c *StationController) List(ctx *app.ListStationContext) error {
-  token := jwt.ContextJWT(ctx)
-  if token == nil {
-    return fmt.Errorf("JWT token is missing from context")
-  }
+	token := jwt.ContextJWT(ctx)
+	if token == nil {
+		return fmt.Errorf("JWT token is missing from context")
+	}
 
-  claims, ok := token.Claims.(jwtgo.MapClaims)
-  if !ok {
-    return fmt.Errorf("JWT claims error")
-  }
+	claims, ok := token.Claims.(jwtgo.MapClaims)
+	if !ok {
+		return fmt.Errorf("JWT claims error")
+	}
 
-  stations := []*data.Station{}
-  if err := c.options.Database.SelectContext(ctx, &stations, "SELECT * FROM fieldkit.station WHERE user_id = $1", claims["sub"]); err != nil {
-    return err
-  }
+	stations := []*data.Station{}
+	if err := c.options.Database.SelectContext(ctx, &stations, "SELECT * FROM fieldkit.station WHERE user_id = $1", claims["sub"]); err != nil {
+		return err
+	}
 
-  return ctx.OK(StationsType(stations))
+	return ctx.OK(StationsType(stations))
 }
--- server/api/station_log.go
+++ server/api/station_log.go
@@ -1,127 +1,99 @@
 package api
 
 import (
-  "fmt"
+	"github.com/goadesign/goa"
 
-  jwtgo "github.com/dgrijalva/jwt-go"
-  "github.com/goadesign/goa"
-  "github.com/goadesign/goa/middleware/security/jwt"
+	"github.com/conservify/sqlxcache"
 
-  "github.com/conservify/sqlxcache"
-
-  "github.com/fieldkit/cloud/server/api/app"
-  "github.com/fieldkit/cloud/server/data"
+	"github.com/fieldkit/cloud/server/api/app"
+	"github.com/fieldkit/cloud/server/data"
 )
 
 type StationLogControllerOptions struct {
-  Database *sqlxcache.DB
+	Database *sqlxcache.DB
 }
 
-func StationLogType(stationlog *data.StationLog) *app.StationLog {
-  return &app.StationLog{
-    ID:           int(stationlog.ID),
-    Body:         stationlog.Body,
-    Timestamp:    stationlog.Timestamp,
-    StationID:    int(stationlog.StationId),
-  }
+func StationLogType(stationLog *data.StationLog) *app.StationLog {
+	return &app.StationLog{
+		ID:        int(stationLog.ID),
+		Body:      stationLog.Body,
+		Timestamp: stationLog.Timestamp,
+		StationID: int(stationLog.StationID),
+	}
 }
 
-func StationLogsType(stationlogs []*data.StationLog) *app.StationLogs {
-  stationLogsCollection := make([]*app.StationLog, len(stationlogs))
-  for i, stationlog := range stationlogs {
-    stationLogsCollection[i] = StationLogType(stationlog)
-  }
-
-  return &app.StationLogs{
-    StationLogs: stationLogsCollection,
-  }
+func StationLogsType(stationLogs []*data.StationLog) *app.StationLogs {
+	stationLogsCollection := make([]*app.StationLog, len(stationLogs))
+	for i, stationLog := range stationLogs {
+		stationLogsCollection[i] = StationLogType(stationLog)
+	}
+	return &app.StationLogs{
+		StationLogs: stationLogsCollection,
+	}
 }
 
-//StationLogController
 type StationLogController struct {
-  *goa.Controller
-  options StationLogControllerOptions
+	*goa.Controller
+	options StationLogControllerOptions
 }
 
-func NewStationLogController(service *goa.Service, options StationLogControllerOptions) * StationLogController {
-  return &StationLogController{
-    Controller: service.NewController("StationLogController"),
-    options:    options,
-  }
+func NewStationLogController(service *goa.Service, options StationLogControllerOptions) *StationLogController {
+	return &StationLogController{
+		Controller: service.NewController("StationLogController"),
+		options:    options,
+	}
 }
 
-func (c *StationLogController) Add(ctx *app.AddStationLogContext) error{
-  token := jwt.ContextJWT(ctx)
-  if token == nil {
-    return fmt.Errorf("JWT token is missing from context")
-  }
+func (c *StationLogController) Add(ctx *app.AddStationLogContext) error {
+	stationLog := &data.StationLog{
+		ID:        int32(ctx.Payload.ID),
+		StationID: int32(ctx.Payload.StationID),
+		Body:      ctx.Payload.Body,
+		Timestamp: ctx.Payload.Timestamp,
+	}
 
-  claims, ok := token.Claims.(jwtgo.MapClaims)
-  if !ok {
-    return fmt.Errorf("JWT claims error")
-  }
+	if err := c.options.Database.NamedGetContext(ctx, stationLog, "INSERT INTO fieldkit.station_log (name, body, timestamp, station_id) VALUES (:name, :body, :timestamp, :station_id) RETURNING *", stationLog); err != nil {
+		return err
+	}
 
-  stationLog := &data.StationLog{
-    ID:         int32(ctx.Payload.ID),
-    StationId:  int32(ctx.Payload.StationID),
-    Body:       ctx.Payload.Body,
-    Timestamp:  ctx.Payload.Timestamp,
-  }
-
-  if err := c.options.Database.NamedGetContext(ctx, stationLog, "INSERT INTO fieldkit.stationlog (name, body, timestamp, station_id) VALUES (:name, :body, :timestamp, :station_id) RETURNING *", stationLog); err != nil {
-    return err
-  }
-
-  if _, err := c.options.Database.ExecContext(ctx, "INSERT INTO fieldkit.stationlog_user (stationlog_id, user_id) VALUES ($1, $2)", stationLog.ID, claims["sub"]); err != nil{
-    return err
-  }
-
-  return ctx.OK(StationLogType(stationLog))
+	return ctx.OK(StationLogType(stationLog))
 }
 
 func (c *StationLogController) AddMultiple(ctx *app.AddMultipleStationLogContext) error {
+	for _, addLog := range ctx.Payload.StationLogs {
+		stationLog := &data.StationLog{
+			StationID: int32(addLog.StationID),
+			Body:      addLog.Body,
+			Timestamp: addLog.Timestamp,
+		}
 
-  for _, add_log := range ctx.Payload.StationLogs {
-    stationLog := &data.StationLog{
-      StationId:  int32(add_log.StationID),
-      Body:       add_log.Body,
-      Timestamp:  add_log.Timestamp,
-    }
+		if err := c.options.Database.NamedGetContext(ctx, stationLog, "INSERT INTO fieldkit.station_log (station_id, body, timestamp) VALUES (:id, :station_id, :body, :timestamp) RETURNING *", stationLog); err != nil {
+			return err
+		}
+	}
 
-    if err := c.options.Database.NamedGetContext(ctx, stationLog, "INSERT INTO fieldkit.stationLog (ID, stationId, Body, Timestamp) VALUES (:ID, :stationId, :Body, :Timestamp) RETURNING *", stationLog); err != nil {
-      return err
-    }
-
-  }
-
-  //return ctx.OK(StationLogsType(stationlogs))
-  /*
-  if _, err := options.Database.ExecContext(ctx, "INSERT INTO fieldkit.stationlog_user ") {
-    return err
-  }
-  */
-  return nil
+	return nil
 }
 
-func (c * StationLogController) Update(ctx *app.UpdateStationLogContext) error {
-  stationlog := &data.StationLog{
-    ID:   int32(ctx.StationLogID),
-    StationId:  int32(ctx.Payload.StationID),
-    Body:       ctx.Payload.Body,
-    Timestamp:  ctx.Payload.Timestamp,
-  }
-  if err := c.options.Database.NamedGetContext(ctx, stationlog, "UPDATE fieldkit.stationlog SET name = :name, station_id = :station_id, body = :body, timestamp = :timestamp, WHERE id = :id RETURNING *", stationlog); err != nil {
-    return err
-  }
+func (c *StationLogController) Update(ctx *app.UpdateStationLogContext) error {
+	stationLog := &data.StationLog{
+		ID:        int32(ctx.StationLogID),
+		StationID: int32(ctx.Payload.StationID),
+		Body:      ctx.Payload.Body,
+		Timestamp: ctx.Payload.Timestamp,
+	}
+	if err := c.options.Database.NamedGetContext(ctx, stationLog, "UPDATE fieldkit.station_log SET name = :name, station_id = :station_id, body = :body, timestamp = :timestamp, WHERE id = :id RETURNING *", stationLog); err != nil {
+		return err
+	}
 
-  return ctx.OK(StationLogType(stationlog))
+	return ctx.OK(StationLogType(stationLog))
 }
 
 func (c *StationLogController) Get(ctx *app.GetStationLogContext) error {
-  stationlog := &data.StationLog{}
-  if err := c.options.Database.GetContext(ctx, stationlog, "SELECT * FROM fieldkit.stationlog WHERE name = $1", ctx.StationLog); err != nil {
-    return err
-  }
+	stationLog := &data.StationLog{}
+	if err := c.options.Database.GetContext(ctx, stationLog, "SELECT * FROM fieldkit.station_log WHERE name = $1", ctx.StationLog); err != nil {
+		return err
+	}
 
-  return ctx.OK(StationLogType(stationlog))
+	return ctx.OK(StationLogType(stationLog))
 }
