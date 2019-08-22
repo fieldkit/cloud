8b25e26bcb25ee4ff30a893cab16a62eb91e8884
Jacob Lewallen <jlewallen@gmail.com>
2019-08-21 18:16:46 -0700

    Moving stuff into tools, renaming.

--- sql/delete-devices.sql
+++
@@ -1,4 +0,0 @@
-delete from fieldkit.record where source_id in (select id from fieldkit.source where name = 'weather-proxy');
-delete from fieldkit.device_location where device_id in (select id from fieldkit.source where name = 'weather-proxy');
-delete from fieldkit.device where source_id in (select id from fieldkit.source where name = 'weather-proxy');
-delete from fieldkit.source where name = 'weather-proxy';
