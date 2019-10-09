825da815a4f46e0b497e8ffa02318cf90dc02b4b
Jacob Lewallen <jlewallen@gmail.com>
2019-10-09 12:08:19 -0700

    Filter broken data, temporarily :)

--- server/backend/repositories/record_repository.go
+++ server/backend/repositories/record_repository.go
@@ -34,7 +34,7 @@ func (r *RecordRepository) QueryDevice(ctx context.Context, deviceId string, pag
 	drs := []*data.DataRecord{}
 	if err := r.Database.SelectContext(ctx, &drs, `
 	    SELECT r.id, r.provision_id, r.time, r.time, r.number, r.meta, ST_AsBinary(r.location) AS location, r.raw FROM fieldkit.data_record AS r JOIN fieldkit.provision AS p ON (r.provision_id = p.id)
-	    WHERE (p.device_id = $1)
+	    WHERE (p.device_id = $1) AND r.time < '10/8/2019'
 	    ORDER BY r.time DESC LIMIT $2 OFFSET $3`, deviceIdBytes, pageSize, pageSize*pageNumber); err != nil {
 		return nil, err
 	}
@@ -43,7 +43,7 @@ func (r *RecordRepository) QueryDevice(ctx context.Context, deviceId string, pag
 	if err := r.Database.SelectContext(ctx, &mrs, `
 	    SELECT m.* FROM fieldkit.meta_record AS m WHERE (m.id IN (
 		SELECT DISTINCT r.meta FROM fieldkit.data_record AS r JOIN fieldkit.provision AS p ON (r.provision_id = p.id)
-		WHERE (p.device_id = $1) LIMIT $2 OFFSET $3
+		WHERE (p.device_id = $1) AND r.time < '10/8/2019' LIMIT $2 OFFSET $3
 	    ))`, deviceIdBytes, pageSize, pageSize*pageNumber); err != nil {
 		return nil, err
 	}
