9ec5996514b8147b2cee66b493ef9253f78ef037
littlecomputer <libbey@conservify.org>
2020-01-22 13:56:01 -0800

    Add categories to field_note_category table

---
+++ migrations/20200122133104_add_more_field_note_categories.down.sql
@@ -0,0 +1,5 @@
+DELETE FROM fieldkit.field_note_category WHERE key='locationName';
+DELETE FROM fieldkit.field_note_category WHERE key='studyObjective';
+DELETE FROM fieldkit.field_note_category WHERE key='locationPurpose';
+DELETE FROM fieldkit.field_note_category WHERE key='siteCriteria';
+DELETE FROM fieldkit.field_note_category WHERE key='siteDescription';
---
+++ migrations/20200122133104_add_more_field_note_categories.up.sql
@@ -0,0 +1,5 @@
+INSERT INTO fieldkit.field_note_category (key, name) VALUES ('locationName', 'Location Name');
+INSERT INTO fieldkit.field_note_category (key, name) VALUES ('studyObjective', 'Study Objective');
+INSERT INTO fieldkit.field_note_category (key, name) VALUES ('locationPurpose', 'Location Purpose');
+INSERT INTO fieldkit.field_note_category (key, name) VALUES ('siteCriteria', 'Site Criteria');
+INSERT INTO fieldkit.field_note_category (key, name) VALUES ('siteDescription', 'Site Description');
