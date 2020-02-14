26ca08ac5959e69b1c416f66c2ba529b4593968f
Jacob Lewallen <jlewallen@gmail.com>
2020-02-14 07:31:06 -0800

    Let defaults in the binary take over.

--- build.sh
+++ build.sh
@@ -86,10 +86,6 @@ echo '.DS_Store
 Dockerfile' > build/.dockerignore
 
 echo 'FROM scratch
-ENV FIELDKIT_ADDR=:80
-ENV FIELDKIT_PORTAL_ROOT=/portal
-ENV FIELDKIT_OCR_PORTAL_ROOT=/ocr-portal
-ENV FIELDKIT_LEGACY_ROOT=/legacy
 COPY . /
 ADD ca-certificates.crt /etc/ssl/certs/
 EXPOSE 80
