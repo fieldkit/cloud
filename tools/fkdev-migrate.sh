c28a0f163baefca5e69eafca1a2d0370c3c4e906
Jacob Lewallen <jlewallen@gmail.com>
2020-02-17 17:00:32 -0800

    None of these are useful anymore.

--- tools/fkdev-migrate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-uname -a
-
-source /etc/docker/compose/fieldkit.env
-
-migrations/migrate.linux-amd64 --path migrations --database $FIELDKIT_POSTGRES_URL up
--- tools/get-lines.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-fk-local.sh "http://127.0.0.1:8080/data/devices/fb51dcff5335353334202020ff182a38/data/lines?pageSize=1000"
