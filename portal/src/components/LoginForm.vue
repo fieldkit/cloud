ffc1832da741a9ed4588a3d39416ceb405e55c1f
littlecomputer <libbey@conservify.org>
2020-04-01 15:46:18 -0700

    Allow "+" in email addresses

--- portal/src/components/LoginForm.vue
+++ portal/src/components/LoginForm.vue
@@ -84,7 +84,7 @@ export default {
                 return;
             }
             // eslint-disable-next-line
-            let emailPattern = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+            let emailPattern = /^([a-zA-Z0-9_+\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
             this.emailNotValid = !emailPattern.test(this.email);
         },
 
