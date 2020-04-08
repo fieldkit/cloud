1e26cb048db943ae099634e25efdab7efd410ff4
littlecomputer <libbey@conservify.org>
2020-04-07 17:08:38 -0700

    Test that URL is updated when "Day" is clicked

--- portal/tests/unit/DataChartControl.spec.js
+++ portal/tests/unit/DataChartControl.spec.js
@@ -1,13 +1,19 @@
-import { mount } from "@vue/test-utils";
+import { createLocalVue, mount } from "@vue/test-utils";
 import DataChartControl from "@/components/DataChartControl.vue";
+import VueRouter from "vue-router";
 
+// some tests use Vue Router with localVue
+const localVue = createLocalVue();
+localVue.use(VueRouter);
+// and some tests use custom mocks for $route and $router
 const $route = {
     query: {},
 };
-
 const $router = {
     push: jest.fn(),
 };
+
+const DAY = 1000 * 60 * 60 * 24;
 
 let wrapper = null;
 
@@ -30,7 +36,6 @@ describe("DataChartControl.vue", () => {
     });
 
     it("Requests one day of data when user selects day", async () => {
-        const DAY = 1000 * 60 * 60 * 24;
         const range = [new Date("1/2/20"), new Date("2/13/20")];
         // new start is one day before the end date:
         const newStart = new Date(range[1].getTime() - DAY);
@@ -49,5 +54,34 @@ describe("DataChartControl.vue", () => {
         await wrapper.vm.$nextTick();
         // this emitted event triggers an api query for the specified range
         expect(wrapper.emitted().timeChanged[0][0].start).toEqual(newStart);
+    });
+
+    it("Updates the url start param when user selects day", async () => {
+        const range = [new Date("1/2/20"), new Date("2/13/20")];
+        // new start is one day before the end date:
+        const newStart = new Date(range[1].getTime() - DAY);
+        const routes = [
+            {
+                path: "/dashboard/data/154",
+                name: "viewData",
+                component: DataChartControl,
+                props: true,
+            },
+        ];
+        const router = new VueRouter({
+            routes,
+        });
+        const wrapper = mount(DataChartControl, {
+            localVue,
+            router,
+            propsData: {
+                station: { name: "FieldKit 1" },
+                totalTime: range,
+            },
+        });
+        await wrapper.vm.$nextTick();
+        wrapper.find("[data-time='1']").trigger("click");
+        await wrapper.vm.$nextTick();
+        expect(wrapper.vm.$route.query.start).toBe(newStart.getTime());
     });
 });
