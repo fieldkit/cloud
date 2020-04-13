3694b8e656c0a8ed0dccbb2e6ea8d1a2d0850528
littlecomputer <libbey@conservify.org>
2020-04-13 16:15:01 -0700

    Setting up test env to work with new treeselect component

--- portal/webpack.config.js
+++ portal/webpack.config.js
@@ -11,23 +11,32 @@ module.exports = {
     mode: "development",
     entry: ["./src/app.js"],
     output: {
-        publicPath: PUBLIC_PATH
+        publicPath: PUBLIC_PATH,
     },
     module: {
+        loaders: [
+            { test: /\.css$/, loader: "css-loader" }
+        ]
         rules: [
             {
                 test: /\.vue$/,
-                use: "vue-loader"
-            }
-        ]
+                use: "vue-loader",
+            },
+            // this will apply to both plain `.scss` files
+            // AND `<style lang="scss">` blocks in `.vue` files
+            {
+                test: /\.scss$/,
+                use: ["vue-style-loader", "css-loader", "sass-loader"],
+            },
+        ],
     },
     plugins: [
         new VueLoaderPlugin(),
         new DefinePlugin({
-            "process.env.PUBLIC_PATH": JSON.stringify(PUBLIC_PATH)
+            "process.env.PUBLIC_PATH": JSON.stringify(PUBLIC_PATH),
         }),
         new webpack.ProvidePlugin({
-            mapboxgl: 'mapbox-gl',
-        })
-    ]
+            mapboxgl: "mapbox-gl",
+        }),
+    ],
 };
