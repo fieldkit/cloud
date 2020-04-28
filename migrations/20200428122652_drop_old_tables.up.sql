88db34816d95d002bfd4cdd777eaef30a2ab0d79
Jacob Lewallen <jlewallen@gmail.com>
2020-04-28 13:40:57 -0700

    Finally drop old tables.

---
+++ migrations/20200428122652_drop_old_tables.up.sql
@@ -0,0 +1,21 @@
+ALTER TABLE fieldkit.twitter_oauth DROP COLUMN source_id;
+
+DROP VIEW  fieldkit.device_locations_view;
+DROP VIEW  fieldkit.record_visible;
+
+DROP TABLE fieldkit.record_analysis;
+DROP TABLE fieldkit.old_record;
+
+DROP TABLE fieldkit.source_twitter_account;
+DROP TABLE fieldkit.device_stream;
+DROP TABLE fieldkit.device_firmware;
+DROP TABLE fieldkit.device_schema;
+DROP TABLE fieldkit.schema;
+DROP TABLE fieldkit.device_notes;
+DROP TABLE fieldkit.device_location;
+DROP TABLE fieldkit.device;
+DROP TABLE fieldkit.source;
+DROP TABLE fieldkit.source_token;
+DROP TABLE fieldkit.team_user;
+DROP TABLE fieldkit.team;
+DROP TABLE fieldkit.expedition;
