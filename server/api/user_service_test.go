261b1525e11ae90c9c2d8a2eb6f2bdfab46106f9
Jacob Lewallen <jlewallen@gmail.com>
2020-05-07 09:29:30 -0700

    Verify validation failures don't log passwords.

--- server/api/user_service_test.go
+++ server/api/user_service_test.go
@@ -61,3 +61,21 @@ func TestGetAvailableRoles(t *testing.T) {
 
 	assert.Equal(http.StatusOK, rr.Code)
 }
+
+func TestLoginPasswordFailsValidation(t *testing.T) {
+	assert := assert.New(t)
+	e, err := tests.NewTestEnv()
+	assert.NoError(err)
+
+	user, err := e.AddUser("goodgoodgood")
+	assert.NoError(err)
+
+	api, err := NewTestableApi(e)
+	assert.NoError(err)
+
+	rbody := strings.NewReader(fmt.Sprintf(`{ "email": "%s", "password": "short" }`, user.Email))
+	req, _ := http.NewRequest("POST", "/login", rbody)
+	rr := tests.ExecuteRequest(req, api)
+
+	assert.Equal(http.StatusBadRequest, rr.Code)
+}
