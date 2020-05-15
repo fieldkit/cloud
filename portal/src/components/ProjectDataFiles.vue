b7f8b3e1c171c66e10e205ddde4ec3f9afe49fde
littlecomputer <libbey@conservify.org>
2020-05-15 16:58:19 -0700

    Placeholder for project data files viz

---
+++ portal/src/components/ProjectDataFiles.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="datafiles-container">
+        <div class="heading">Data Files</div>
+    </div>
+</template>
+
+<script>
+import FKApi from "../api/api";
+
+export default {
+    name: "ProjectDataFiles",
+    data: () => {
+        return {};
+    },
+    props: ["project"],
+    async beforeCreate() {
+        this.api = new FKApi();
+    },
+    mounted() {},
+    methods: {},
+};
+</script>
+
+<style scoped>
+.datafiles-container {
+    position: relative;
+    float: left;
+    width: 582px;
+    height: 300px;
+    margin: 22px 22px 0 0;
+    border: 1px solid #d8dce0;
+}
+.heading {
+    font-size: 20px;
+    font-weight: 600;
+    float: left;
+    margin: 25px;
+}
+</style>
