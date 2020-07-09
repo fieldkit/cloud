8b0b9d0b2ac8ee7bf49b758649a3fd26ea5ff6f3
Jacob Lewallen <jlewallen@gmail.com>
2020-07-09 15:09:09 -0700

    A fake sensor data generator for testing agg and viz.

--- server/cmd/fkdata/fkdata.go
+++ server/cmd/fkdata/fkdata.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"os"
+	"math"
 	"time"
 
 	"github.com/kelseyhightower/envconfig"
@@ -14,6 +14,7 @@ import (
 	"github.com/conservify/sqlxcache"
 
 	"github.com/fieldkit/cloud/server/backend"
+	"github.com/fieldkit/cloud/server/backend/handlers"
 	"github.com/fieldkit/cloud/server/common/logging"
 )
 
@@ -21,10 +22,64 @@ type Options struct {
 	StationID int
 	All       bool
 	Recently  bool
+	Fake      bool
 }
 
 type Config struct {
 	PostgresURL string `split_words:"true" default:"postgres://localhost/fieldkit?sslmode=disable" required:"true"`
+}
+
+func main() {
+	options := &Options{}
+
+	flag.IntVar(&options.StationID, "station-id", 0, "station id")
+	flag.BoolVar(&options.All, "all", false, "all stations")
+	flag.BoolVar(&options.Recently, "recently", false, "recently inserted data")
+	flag.BoolVar(&options.Fake, "fake", false, "create a fake sine wave")
+
+	flag.Parse()
+
+	config := &Config{}
+	if err := envconfig.Process("FIELDKIT", config); err != nil {
+		panic(err)
+	}
+
+	db, err := sqlxcache.Open("postgres", config.PostgresURL)
+	if err != nil {
+		panic(err)
+	}
+
+	logging.Configure(false, "fkdata")
+
+	ctx := context.Background()
+
+	log := logging.Logger(ctx).Sugar()
+
+	if options.StationID > 0 {
+		if options.Fake {
+			if err := generateFake(ctx, db, int32(options.StationID)); err != nil {
+				panic(err)
+			}
+		} else {
+			if err := processStation(ctx, db, int32(options.StationID), options.Recently); err != nil {
+				panic(err)
+			}
+		}
+	}
+
+	if options.All {
+		ids := []*IDRow{}
+		if err := db.SelectContext(ctx, &ids, `SELECT id FROM fieldkit.station`); err != nil {
+			panic(err)
+		}
+
+		for _, id := range ids {
+			log.Infow("station", "station_id", id.ID)
+			if err := processStation(ctx, db, int32(id.ID), options.Recently); err != nil {
+				log.Errorw("error", "station_id", id.ID, "error", err)
+			}
+		}
+	}
 }
 
 func processStation(ctx context.Context, db *sqlxcache.DB, stationID int32, recently bool) error {
@@ -46,55 +101,47 @@ func processStation(ctx context.Context, db *sqlxcache.DB, stationID int32, rece
 	return nil
 }
 
-func main() {
-	options := &Options{}
+func generateFake(ctx context.Context, db *sqlxcache.DB, stationID int32) error {
+	sampled := time.Now().Add(-100 * 24 * time.Hour)
+	end := time.Now()
+	interval := time.Minute * 1
 
-	flag.IntVar(&options.StationID, "station-id", 0, "station id")
-	flag.BoolVar(&options.All, "all", false, "all stations")
-	flag.BoolVar(&options.Recently, "recently", false, "recently inserted data")
+	aggregator := handlers.NewAggregator(db, stationID, 1000)
 
-	flag.Parse()
+	sensorKey := "fk.testing.sin"
 
-	if options.StationID == 0 && !options.All {
-		flag.Usage()
-		os.Exit(2)
+	SecondsPerWeek := int64(60 * 60 * 24 * 7)
+
+	getValue := func(t time.Time) float64 {
+		scaled := float64(SecondsPerWeek) / float64(t.Unix()%SecondsPerWeek)
+		radians := scaled * math.Pi
+		return math.Sin(radians)
 	}
 
-	config := &Config{}
-	if err := envconfig.Process("FIELDKIT", config); err != nil {
-		panic(err)
-	}
+	for sampled.Before(end) {
+		value := getValue(sampled)
 
-	db, err := sqlxcache.Open("postgres", config.PostgresURL)
-	if err != nil {
-		panic(err)
-	}
-
-	logging.Configure(false, "fkdata")
-
-	ctx := context.Background()
-
-	log := logging.Logger(ctx).Sugar()
-
-	if options.StationID > 0 {
-		if err := processStation(ctx, db, int32(options.StationID), options.Recently); err != nil {
-			panic(err)
-		}
-	}
-
-	if options.All {
-		ids := []*IDRow{}
-		if err := db.SelectContext(ctx, &ids, `SELECT id FROM fieldkit.station`); err != nil {
-			panic(err)
+		if err := aggregator.NextTime(ctx, sampled); err != nil {
+			return fmt.Errorf("error adding: %v", err)
 		}
 
-		for _, id := range ids {
-			log.Infow("station", "station_id", id.ID)
-			if err := processStation(ctx, db, int32(id.ID), options.Recently); err != nil {
-				log.Errorw("error", "station_id", id.ID, "error", err)
+		if err := db.WithNewTransaction(ctx, func(txCtx context.Context) error {
+			if err := aggregator.AddSample(txCtx, sampled, []float64{}, sensorKey, value); err != nil {
+				return err
 			}
+			return nil
+		}); err != nil {
+			return err
 		}
+
+		sampled = sampled.Add(interval)
 	}
+
+	if err := aggregator.Close(ctx); err != nil {
+		return nil
+	}
+
+	return nil
 }
 
 type IDRow struct {
