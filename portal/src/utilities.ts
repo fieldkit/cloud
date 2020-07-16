6d8676eca22ca4bc518df2cb06d2ac0c5cb0c321
Jacob Lewallen <jlewallen@gmail.com>
2020-07-16 16:24:38 -0700

    Allow serialized chains to just return values.

--- portal/src/utilities.ts
+++ portal/src/utilities.ts
@@ -22,7 +22,7 @@ export function getUpdatedDate(station) {
 export function serializePromiseChain(all, fn) {
     return all.reduce((accum, value, index) => {
         return accum.then((allValues) => {
-            return fn(value, index).then((singleValue) => {
+            return Promise.resolve(fn(value, index)).then((singleValue) => {
                 allValues.push(singleValue);
                 return allValues;
             });
