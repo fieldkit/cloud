b6088b96231b2f6f3d495b90220d33b1c459bd83
Jacob Lewallen <jlewallen@gmail.com>
2020-08-08 17:14:47 -0700

    Querying portion of CSV exporting.

--- server/api/csv_service.go
+++ server/api/csv_service.go
@@ -23,6 +23,19 @@ import (
 	_ "github.com/fieldkit/cloud/server/data"
 )
 
+func NewRawQueryParamsFromCsvExport(payload *csvService.ExportPayload) (*RawQueryParams, error) {
+	return &RawQueryParams{
+		Start:      payload.Start,
+		End:        payload.End,
+		Resolution: payload.Resolution,
+		Stations:   payload.Stations,
+		Sensors:    payload.Sensors,
+		Aggregate:  payload.Aggregate,
+		Tail:       payload.Tail,
+		Complete:   payload.Complete,
+	}, nil
+}
+
 type CsvService struct {
 	options *ControllerOptions
 	db      *sqlxcache.DB
@@ -36,6 +49,46 @@ func NewCsvService(ctx context.Context, options *ControllerOptions) *CsvService 
 }
 
 func (c *CsvService) Export(ctx context.Context, payload *csvService.ExportPayload) (*csvService.ExportResult, error) {
+	log := Logger(ctx).Sugar()
+
+	rawParams, err := NewRawQueryParamsFromCsvExport(payload)
+	if err != nil {
+		return nil, err
+	}
+
+	qp, err := rawParams.BuildQueryParams()
+	if err != nil {
+		return nil, err
+	}
+
+	if len(qp.Sensors) == 0 {
+		return nil, errors.New("sensors is required")
+	}
+
+	log.Infow("query_parameters", "start", qp.Start, "end", qp.End, "sensors", qp.Sensors, "stations", qp.Stations, "resolution", qp.Resolution, "aggregate", qp.Aggregate)
+
+	aggregateName := "1m"
+	aqp, err := NewAggregateQueryParams(qp, aggregateName, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	dq := NewDataQuerier(c.db)
+
+	queried, err := dq.QueryAggregate(ctx, aqp)
+	if err != nil {
+		return nil, err
+	}
+
+	defer queried.Close()
+
+	for queried.Next() {
+		row := &DataRow{}
+		if err = queried.StructScan(row); err != nil {
+			return nil, err
+		}
+	}
+
 	return &csvService.ExportResult{
 		Object: nil,
 	}, nil
