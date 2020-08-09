4aab0234641782abaa0de5bac4911f9e6e0990fa
Jacob Lewallen <jlewallen@gmail.com>
2020-08-09 10:25:29 -0700

    More conventional JSON for messages.

--- server/messages/ingestion_received.go
+++ server/messages/ingestion_received.go
@@ -1,7 +1,7 @@
 package messages
 
 type IngestionReceived struct {
-	QueuedID int64
-	Verbose  bool
-	UserID   int32
+	QueuedID int64 `json:"queued_id"`
+	Verbose  bool  `json:"verbose"`
+	UserID   int32 `json:"user_id"`
 }
--- server/messages/refresh_station.go
+++ server/messages/refresh_station.go
@@ -5,9 +5,9 @@ import (
 )
 
 type RefreshStation struct {
-	StationID   int32
-	Completely  bool
-	Verbose     bool
-	UserID      int32
-	HowRecently time.Duration
+	StationID   int32         `json:"station_id"`
+	Completely  bool          `json:"completely"`
+	Verbose     bool          `json:"verbose"`
+	UserID      int32         `json:"user_id"`
+	HowRecently time.Duration `json:"how_recently"`
 }
