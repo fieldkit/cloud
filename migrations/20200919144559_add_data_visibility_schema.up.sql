d7652e6cf7812701e09f0e70b3c4420a0c463a89
Jacob Lewallen <jlewallen@gmail.com>
2020-09-20 10:54:20 -0700

    Quick schema for storing visibility windows for stations.

---
+++ migrations/20200919144559_add_data_visibility_schema.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE fieldkit.data_visibility (
+	id serial PRIMARY KEY,
+	start_time timestamp NOT NULL,
+	end_time timestamp NOT NULL,
+	station_id integer REFERENCES fieldkit.station (id) NOT NULL,
+	project_id integer REFERENCES fieldkit.project (id),
+	user_id integer REFERENCES fieldkit.user (id)
+);
