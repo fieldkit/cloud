39eff60ab4aff10175b8bc11e2a893ec680b2081
Jacob Lewallen <jlewallen@gmail.com>
2020-09-22 07:48:46 -0700

    Use shorter max time value for readability.

--- server/data/values.go
+++ server/data/values.go
@@ -5,6 +5,6 @@ import (
 )
 
 var (
-	MinimumTime = time.Date(1000, time.January, 0, 0, 0, 0, 0, time.UTC)
-	MaximumTime = time.Date(100000, time.January, 0, 0, 0, 0, 0, time.UTC)
+	MinimumTime = time.Date(1000, time.January, 12, 0, 0, 0, 0, time.UTC)
+	MaximumTime = time.Date(9000, time.January, 12, 0, 0, 0, 0, time.UTC)
 )
