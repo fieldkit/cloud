dc3b20c256b2bc99dc00e25ada9cbf3febbbec51
Jacob Lewallen <jlewallen@gmail.com>
2021-02-09 15:18:25 -0800

    Import realm with some configuration for developer keycloak instance.

--- tools/keycloak.sh
+++ tools/keycloak.sh
@@ -5,8 +5,9 @@ if [ -d ~/conservify/dev-ops/amis/services/auth-keycloak/theme ]; then
 		--link cloud_postgres_1 --network cloud_default \
 		-p 9990:9990 \
 		-p 8090:8080 \
-		-e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin -e KEYCLOAK_DEFAULT_THEME=fk -e KEYCLOAK_LOGLEVEL=DEBUG \
+		-e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin -e KEYCLOAK_LOGLEVEL=DEBUG \
 		-e DB_VENDOR=postgres -e DB_USER=fieldkit -e DB_PASSWORD=password -e DB_ADDR=cloud_postgres_1:5432 \
+		-e KEYCLOAK_IMPORT=/tmp/keycloak-dev-realm.json -v `pwd`/keycloak-dev-realm.json:/tmp/keycloak-dev-realm.json \
 		-v ~/conservify/dev-ops/amis/services/auth-keycloak/theme:/opt/jboss/keycloak/themes/fk \
 		conservify/auth-keycloak:active
 fi
