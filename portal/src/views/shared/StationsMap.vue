18790a98989a6e0a7657313dd8905ce7583c9755
Andrei Radoi <andrei.radoi@neurony.ro>
2021-08-19 12:57:44 +0300

    fix: remove promise for map not loading compass icon

--- portal/src/views/shared/StationsMap.vue
+++ portal/src/views/shared/StationsMap.vue
@@ -101,42 +101,25 @@ export default Vue.extend({
             console.log("map: loaded");
             this.protectedData.map = map;
 
-            this.loadCompassImage(map)
-                .then(() => {
-                    setTimeout(() => {
-                        console.log("ressize");
-                        map.resize();
-
-                        this.ready = true;
-                        this.updateMap();
-
-                        // Force model to update.
-                        this.newBounds();
-                    }, 100);
-                })
-                .catch((error) => {
-                    throw error;
+            if (!map.hasImage("dot")) {
+                const compass = this.$loadAsset("Icon_Map_Dot.png");
+                map.loadImage(compass, (error, image) => {
+                    if (error) throw error;
+                    if (!map.hasImage("dot")) {
+                        map.addImage("dot", image);
+                    }
                 });
-        },
-        loadCompassImage(map) {
-            // return new Promise((resolve => resolve()));
-            return new Promise((resolve, reject) => {
-                console.log('map.hasImage("dot")', map.hasImage("dot"));
-                if (!map.hasImage("dot")) {
-                    const compass = this.$loadAsset("Icon_Map_Dot.png");
-                    map.loadImage(compass, (error, image) => {
-                        console.log("loaded", image);
-                        console.log("loaded e", error);
-                        if (error) reject(error);
-                        if (!map.hasImage("dot")) {
-                            console.log("adding img");
-                            map.addImage("dot", image);
-                            resolve();
-                        }
-                    });
-                }
-                resolve();
-            });
+            }
+
+            setTimeout(() => {
+                map.resize();
+
+                this.ready = true;
+                this.updateMap();
+
+                // Force model to update.
+                this.newBounds();
+            }, 100);
         },
         newBounds() {
             const map = this.protectedData.map;
