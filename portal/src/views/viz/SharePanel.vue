8c3d31aeb8a6e30eb4b8042044cfea0733cd5dd7
Jacob Lewallen <jlewallen@gmail.com>
2022-03-09 10:54:03 -0800

    fix: Less URL-looking name in text.

--- portal/src/views/viz/SharePanel.vue
+++ portal/src/views/viz/SharePanel.vue
@@ -44,7 +44,7 @@ export default Vue.extend({
         twitterUrl(): string {
             const qs = new URLSearchParams();
             qs.append("url", this.vizUrl);
-            qs.append("text", "Check out this data on FieldKit.org");
+            qs.append("text", "Check out this data on FieldKit!");
             return `https://twitter.com/intent/tweet?${qs.toString()}`;
         },
         vizUrl(): string {
