cffadba289d7d26ba4183114d35afd32f8165696
Tyson Anderson <tyson@tysonanderson.com>
2022-03-09 19:18:36 -0700

    fix: fix marker value issue

--- portal/src/views/shared/StationsMap.vue
+++ portal/src/views/shared/StationsMap.vue
@@ -223,7 +223,7 @@ export default Vue.extend({
                 
                 const instance = new valueMarker({
                     propsData: { color: feature.properties.color,
-                                value: 8,
+                                value: feature.properties.value,
                                 id: feature.properties.id },
                 })
                 instance.$mount()
