e4b84c9d4fbc6114cb4acdcafd634ce576fa1da9
Tyson Anderson <tyson@tysonanderson.com>
2022-03-23 03:43:06 -0600

    fix: add user photo and view data icon

--- portal/src/views/shared/LatestEvents.vue
+++ portal/src/views/shared/LatestEvents.vue
@@ -1,23 +1,31 @@
 <template>
     <div class="latest-events">
         <div class="event-row">
-            <div class="event-container" v-for="annotation, idx in latestEvents" :key="idx">
-                <h3>{{ annotation.body }}</h3>
+            <div class="event-container" v-for="(annotation, idx) in displayEvents" :key="idx">
+                <h3 class="event-title">{{ annotation.body }}</h3>
                 <div>{{ annotation.start | date }} &ndash; {{ annotation.end | date }}</div>
-                <img alt="View Data" class="data-icon" src="@/assets/view-data.svg" width="18" />
-                <router-link class="data-link" :to="{name: 'exploreBookmark', query: { bookmark: annotation.bookmark }}">View data</router-link>
-                <UserPhoto :user="annotation.user"></UserPhoto>
-                <div>{{ annotation.user.name }}</div>
+                <div class="row">
+                    <div class="data-link">
+                        <img alt="View Data" class="data-icon" src="@/assets/view-data.svg" width="18" />
+                        <router-link class="data-link" :to="{ name: 'exploreBookmark', query: { bookmark: annotation.bookmark } }">
+                            View data
+                        </router-link>
+                    </div>
+                    <div class="user-details">
+                        <UserPhoto :user="annotation.user"></UserPhoto>
+                        <span class="user-name">{{ annotation.user.name }}</span>
+                    </div>
+                </div>
             </div>
         </div>
-        <pagination-controls :page="0" :total-pages="4"/>
+        <pagination-controls :page="currentPage" :total-pages="numPages" @new-page="onNewPage" />
     </div>
 </template>
 
 <script lang="ts">
 import Vue, { PropType } from "vue";
 import CommonComponents from "@/views/shared";
-import { Annotation} from "@/store";
+import { Annotation } from "@/store";
 // fixme: moment is huge, load module?
 import moment from "moment";
 import PaginationControls from "./PaginationControls.vue";
@@ -28,26 +36,37 @@ export default Vue.extend({
         PaginationControls,
         ...CommonComponents,
     },
-    props: {
-
-    },
+    props: {},
     data(): {
         viewType: string;
+        currentPage: number;
     } {
         return {
-            viewType: "project"
+            viewType: "project",
+            currentPage: 0,
         };
     },
     computed: {
         latestEvents(): Annotation[] {
-            return this.$getters.getEvents.slice(0,4);
+            return this.$getters.getEvents;
+        },
+        numPages(): number {
+            return Math.ceil(this.latestEvents.length / 4);
+        },
+        displayEvents(): Annotation[] {
+            return this.latestEvents.slice(this.currentPage, this.currentPage + 4);
+        },
+    },
+    methods: {
+        onNewPage(evt) {
+            this.currentPage = evt;
         },
     },
     filters: {
         date: (value) => {
-            return moment(value).format('M/D/YYYY, h:mm:ss a');
-        }
-    }
+            return moment(value).format("M/D/YYYY, h:mm:ss a");
+        },
+    },
 });
 </script>
 
@@ -67,12 +86,30 @@ export default Vue.extend({
     border-radius: 2px;
     border: solid 1px var(--color-border);
 }
-
+.event-title {
+    max-width: 100%;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    height: 1.1em;
+    overflow: hidden;
+}
 .data-icon {
     fill: red;
     margin: 10px;
 }
 .data-link {
     font-size: 14px;
+    display: flex;
+    align-items: center;
+}
+.user-details {
+    display: flex;
+    align-items: center;
+    margin-right: 1em;
+}
+.row {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
 }
 </style>
