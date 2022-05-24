b444f52b285c7ab6814623cf2b5bc1b81ca29918
Jacob Lewallen <jlewallen@gmail.com>
2022-05-24 11:50:35 -0700

    feat: Update interestingness on incoming reading.

---
+++ server/backend/repositories/sensors_repository.go
@@ -0,0 +1,34 @@
+package repositories
+
+import (
+	"context"
+
+	"github.com/conservify/sqlxcache"
+
+	"github.com/fieldkit/cloud/server/data"
+)
+
+type SensorsRepository struct {
+	db *sqlxcache.DB
+}
+
+func NewSensorsRepository(db *sqlxcache.DB) (rr *SensorsRepository) {
+	return &SensorsRepository{db: db}
+}
+
+func (r *SensorsRepository) QueryAllSensors(ctx context.Context) (map[string]*data.Sensor, error) {
+	sensors := []*data.Sensor{}
+	if err := r.db.SelectContext(ctx, &sensors,
+		`SELECT id, key, interestingness_priority
+		FROM fieldkit.aggregated_sensor ORDER BY key`); err != nil {
+		return nil, err
+	}
+
+	mapped := make(map[string]*data.Sensor)
+
+	for _, sensor := range sensors {
+		mapped[sensor.Key] = sensor
+	}
+
+	return mapped, nil
+}
