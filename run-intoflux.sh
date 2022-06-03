2f005168f37408ec34a9acfba147797ecd030bb3
Jacob Lewallen <jlewallen@gmail.com>
2022-06-03 09:28:38 -0700

    refact: Faster pagination when processing WebHook messages.

--- run-intoflux.sh
+++ run-intoflux.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+export FIELDKIT_POSTGRES_URL="postgres://fieldkit:password@127.0.0.1/fieldkit?sslmode=disable"
+
 export FIELDKIT_INFLUX_DB_TOKEN="RxdlPKjlsec_NdsTabjga3qNxBU0nAAGEuUMSZVcWX-_R3VXi1YTAtku5fZ5cO8LcbooNIh6qrmjrZRZNqVAOQ=="
+export FIELDKIT_INFLUX_DB_URL="http://127.0.0.1:8086"
+export FIELDKIT_INFLUX_DB_ORG="fk"
+export FIELDKIT_INFLUX_DB_BUCKET="sensors"
 
 make intoflux && time build/intoflux "$@"
 
--- server/webhook/messages_repository.go
+++ server/webhook/messages_repository.go
@@ -3,7 +3,6 @@ package webhook
 import (
 	"context"
 	"database/sql"
-	"math"
 	"time"
 
 	"github.com/conservify/sqlxcache"
@@ -33,8 +32,10 @@ type MessageBatch struct {
 	Messages  []*WebHookMessage
 	Schemas   map[int32]*MessageSchemaRegistration
 	StartTime time.Time
-	page      int32
-	pages     int32
+	maximumID int64
+	totalRows int64
+	rows      int64
+	id        int64
 }
 
 func (rr *MessagesRepository) processQuery(ctx context.Context, batch *MessageBatch, messages []*WebHookMessage) error {
@@ -44,8 +45,10 @@ func (rr *MessagesRepository) processQuery(ctx context.Context, batch *MessageBa
 		return sql.ErrNoRows
 	}
 
+	last := messages[len(messages)-1]
+	batch.rows += int64(len(messages))
+	batch.id = last.ID
 	batch.Messages = messages
-	batch.page += 1
 
 	schemas := NewMessageSchemaRepository(rr.db)
 	_, err := schemas.QuerySchemas(ctx, batch)
@@ -56,68 +59,55 @@ func (rr *MessagesRepository) processQuery(ctx context.Context, batch *MessageBa
 	return nil
 }
 
-func (rr *MessagesRepository) QueryBatchForProcessing(ctx context.Context, batch *MessageBatch) error {
-	log := Logger(ctx).Sugar()
-
-	if batch.page == 0 {
-		summary := &BatchSummary{}
-		if err := rr.db.GetContext(ctx, summary, `
-			SELECT COUNT(id) AS total, MIN(created_at) AS start, MAX(created_at) AS end
-			FROM fieldkit.ttn_messages WHERE NOT ignored
-			`); err != nil {
-			return err
-		}
-
-		batch.pages = int32(math.Ceil(float64(summary.Total) / float64(BatchSize)))
-	}
-
-	progress := float32(batch.page) / float32(batch.pages) * 100.0
-
-	log.Infow("querying", "start", batch.StartTime, "page", batch.page, "pages", batch.pages, "progress", progress)
-
-	messages := []*WebHookMessage{}
-	if err := rr.db.SelectContext(ctx, &messages, `
-		SELECT id, created_at, schema_id, headers, body FROM fieldkit.ttn_messages
-		WHERE schema_id IS NOT NULL AND NOT ignored
-		ORDER BY created_at LIMIT $1 OFFSET $2
-		`, BatchSize, batch.page*BatchSize); err != nil {
-		return err
-	}
-	return rr.processQuery(ctx, batch, messages)
-}
-
 type BatchSummary struct {
-	Total int64      `db:"total"`
-	Start *time.Time `db:"start"`
-	End   *time.Time `db:"end"`
+	Total     int64      `db:"total"`
+	MaximumID int64      `db:"maximum_id"`
+	Start     *time.Time `db:"start"`
+	End       *time.Time `db:"end"`
 }
 
 func (rr *MessagesRepository) QueryBatchBySchemaIDForProcessing(ctx context.Context, batch *MessageBatch, schemaID int32) error {
 	log := Logger(ctx).Sugar()
 
-	if batch.page == 0 {
+	if batch.id == 0 {
+		log.Infow("counting")
+
 		summary := &BatchSummary{}
 		if err := rr.db.GetContext(ctx, summary, `
-			SELECT COUNT(id) AS total, MIN(created_at) AS start, MAX(created_at) AS end
+			SELECT MAX(id) AS maximum_id, COUNT(id) AS total, MIN(created_at) AS start, MAX(created_at) AS end
 			FROM fieldkit.ttn_messages WHERE (schema_id = $1) AND (created_at >= $2) AND NOT ignored
 			`, schemaID, batch.StartTime); err != nil {
 			return err
 		}
 
-		batch.pages = int32(math.Ceil(float64(summary.Total) / float64(BatchSize)))
+		batch.totalRows = summary.Total
+		batch.maximumID = summary.MaximumID
+
+		log.Infow("counted", "total_rows", batch.totalRows, "maximum_id", batch.maximumID)
 	}
 
-	progress := float32(batch.page) / float32(batch.pages) * 100.0
+	if batch.id >= batch.maximumID {
+		return sql.ErrNoRows
+	}
 
-	log.Infow("querying", "start", batch.StartTime, "page", batch.page, "pages", batch.pages, "progress", progress)
+	progress := float32(batch.rows) / float32(batch.totalRows) * 100.0
+
+	log.Infow("querying", "start", batch.StartTime, "id", batch.id, "maximum_id", batch.maximumID, "rows", batch.rows, "total_rows", batch.totalRows, "progress", progress)
+
+	started := time.Now()
 
 	messages := []*WebHookMessage{}
 	if err := rr.db.SelectContext(ctx, &messages, `
 		SELECT id, created_at, schema_id, headers, body FROM fieldkit.ttn_messages
-		WHERE (schema_id = $1) AND (created_at >= $4) AND NOT ignored
-		ORDER BY created_at ASC LIMIT $2 OFFSET $3
-		`, schemaID, BatchSize, batch.page*BatchSize, batch.StartTime); err != nil {
+		WHERE (id > $1) AND (schema_id = $2) AND (created_at >= $3) AND NOT ignored
+		ORDER BY id ASC LIMIT $4
+		`, batch.id, schemaID, batch.StartTime, BatchSize); err != nil {
 		return err
 	}
+
+	elapsed := time.Since(started)
+
+	log.Infow("queried", "elapsed", elapsed, "records", len(messages))
+
 	return rr.processQuery(ctx, batch, messages)
 }
