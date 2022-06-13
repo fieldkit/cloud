dee5175b7774439cdba39c4a5674226e17b7b51c
Jacob Lewallen <jlewallen@gmail.com>
2022-06-13 15:01:39 -0700

    refact: Simple windowing support.

--- server/api/sensor_service.go
+++ server/api/sensor_service.go
@@ -409,6 +409,10 @@ func NewInfluxDBBackend(config *InfluxDBConfig) (*InfluxDBBackend, error) {
 }
 
 func (idb *InfluxDBBackend) QueryData(ctx context.Context, qp *backend.QueryParams) (*QueriedData, error) {
+	if len(qp.Stations) != 1 {
+		return nil, fmt.Errorf("multiple stations unsupported")
+	}
+
 	log := Logger(ctx).Sugar()
 
 	query := fmt.Sprintf(`
@@ -417,10 +421,11 @@ func (idb *InfluxDBBackend) QueryData(ctx context.Context, qp *backend.QueryPara
 		|> filter(fn: (r) => r._measurement == "reading")
 		|> filter(fn: (r) => r._field == "value")
 		|> filter(fn: (r) => r.station_id == "%v")
+		|> aggregateWindow(every: 12h, fn: max)
 		|> yield(name: "max")
 	`, idb.config.Bucket, qp.Start.Unix(), qp.End.Unix(), qp.Stations[0])
 
-	log.Infow("influx:querying", "query", query)
+	log.Infow("influx:querying", "query", query, "start", qp.Start, "end", qp.End)
 
 	rows, err := idb.queryAPI.Query(ctx, query)
 	if err != nil {
@@ -433,38 +438,55 @@ func (idb *InfluxDBBackend) QueryData(ctx context.Context, qp *backend.QueryPara
 
 	for rows.Next() {
 		record := rows.Record()
-
 		time := record.Time()
-		value := record.Value()
 
-		if floatValue, ok := value.(float64); !ok {
-			log.Infow("influx:unexpected", "value", value)
-		} else {
-			values := record.Values()
+		rawValue := record.Value()
+		var floatValue *float64
+		if rawValue != nil {
+			if maybeFloatValue, ok := rawValue.(float64); !ok {
+				log.Infow("influx:unexpected", "value", rawValue)
+			} else {
+				floatValue = &maybeFloatValue
+			}
+		}
 
-			stationIDString := values["station_id"].(string)
-			sensorIDString := values["sensor_id"].(string)
-			moduleID := values["module_id"].(string)
+		values := record.Values()
+		stationIDRaw := values["station_id"]
+		sensorIDRaw := values["sensor_id"]
+		moduleIDRaw := values["module_id"]
 
-			stationID, stationOk := strconv.Atoi(stationIDString)
+		row := &backend.DataRow{
+			Time:      data.NumericWireTime(time),
+			Value:     nil,
+			StationID: nil,
+			SensorID:  nil,
+			ModuleID:  nil,
+			Location:  nil,
+		}
 
-			sensorID, sensorOk := strconv.Atoi(sensorIDString)
-
+		if stationIDRaw != nil && sensorIDRaw != nil && moduleIDRaw != nil {
+			stationID, stationOk := strconv.Atoi(stationIDRaw.(string))
+			sensorID, sensorOk := strconv.Atoi(sensorIDRaw.(string))
+			moduleID := moduleIDRaw.(string)
 			if sensorOk == nil && stationOk == nil {
 				stationIDi32 := int32(stationID)
 				sensorIDi64 := int64(sensorID)
-				dataRows = append(dataRows, &backend.DataRow{
+				row = &backend.DataRow{
 					Time:      data.NumericWireTime(time),
-					Value:     &floatValue,
+					Value:     floatValue,
 					StationID: &stationIDi32,
 					SensorID:  &sensorIDi64,
 					ModuleID:  &moduleID,
 					Location:  nil,
-				})
+				}
 			} else {
 				log.Infow("influx:unexpected", "values", values)
 			}
+		} else {
+			log.Infow("influx:unexpected", "values", values)
 		}
+
+		dataRows = append(dataRows, row)
 	}
 
 	if rows.Err() != nil {
