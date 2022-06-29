0d97421b77b2b621df8221cc7f948f9bd3ee1482
Jacob Lewallen <jlewallen@gmail.com>
2022-06-29 15:11:14 -0700

    feat: Work in progress schema for TsDB.

---
+++ tsdb/00_schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE fieldkit.sensor_data (
+    time TIMESTAMPTZ NOT NULL,
+    station_id INTEGER NOT NULL /* REFERENCES fieldkit.station (id) */,
+    module_id INTEGER NOT NULL /* REFERENCES fieldkit.station_module (id) */,
+    sensor_id INTEGER NOT NULL /* REFERENCES fieldkit.aggregated_sensor (id) */,
+    value FLOAT NOT NULL
+);
+
+SELECT create_hypertable('fieldkit.sensor_data', 'time');
+
+CREATE INDEX sensor_data_idx ON fieldkit.sensor_data (station_id, module_id, sensor_id, time DESC);
