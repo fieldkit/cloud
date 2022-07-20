06b9b35fcf4861cd934a45ad2df25f9eed77fe98
Jacob Lewallen <jlewallen@gmail.com>
2022-07-20 11:43:33 -0700

    refact: Don't bundle migrations into image.

--- migrations/main.go
+++ migrations/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -15,27 +14,14 @@ import (
 	migrations "github.com/robinjoseph08/go-pg-migrations/v3"
 )
 
-const directory = "migrations"
-
-type options struct {
-	Path       string
-	SearchPath string
-}
-
 func main() {
-	o := &options{}
-
-	flag.StringVar(&o.Path, "path", "./", "path to migrations")
-
-	flag.Parse()
-
 	options := &pg.Options{
 		Addr:     "",
 		User:     "",
 		Database: "",
 		Password: "",
 		OnConnect: func(ctx context.Context, conn *pg.Conn) error {
-			log.Printf("creating schema...")
+			log.Printf("Creating schema...")
 
 			if _, err := conn.Exec("CREATE SCHEMA IF NOT EXISTS fieldkit"); err != nil {
 				return fmt.Errorf("error creating: %v", err)
@@ -49,13 +35,13 @@ func main() {
 				return fmt.Errorf("error granting: %v", err)
 			}
 
-			log.Printf("done creating schema...")
+			log.Printf("Done creating schema...")
 
 			return nil
 		},
 	}
 
-	url := os.Getenv("PGURL")
+	url := os.Getenv("MIGRATE_DATABASE_URL")
 	if url != "" {
 		o, err := pg.ParseURL(url)
 		if err != nil {
@@ -65,10 +51,16 @@ func main() {
 		options = o
 	}
 
-	files, err := filepath.Glob(filepath.Join(o.Path, "*.up.sql"))
+	directory := "/migrations"
+
+	log.Printf("Scanning %s...", directory)
+
+	files, err := filepath.Glob(filepath.Join(directory, "*.up.sql"))
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	log.Printf("Found %d migration(s) in %s...", len(files), directory)
 
 	for _, file := range files {
 		data, err := ioutil.ReadFile(file)
@@ -89,7 +81,9 @@ func main() {
 
 		opts := migrations.MigrationOptions{}
 
-		migrations.Register(file, up, down, opts)
+		_, fileOnly := filepath.Split(file)
+
+		migrations.Register(fileOnly, up, down, opts)
 	}
 
 	db := pg.Connect(options)
