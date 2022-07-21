57a6143f8b274fa4c38534a73e8eb6fb79d97170
Jacob Lewallen <jlewallen@gmail.com>
2022-07-21 14:49:56 -0700

    feat: Update value on conflict for movedata/AH handler.
    This requires us to use batches instead of CopyFrom, which is slightly
    slower. Oh well.

--- run-movedata.sh
+++ run-movedata.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 export FIELDKIT_POSTGRES_URL="postgres://fieldkit:password@127.0.0.1/fieldkit?sslmode=disable"
+export FIELDKIT_TIME_SCALE_URL="postgres://postgres:password@127.0.0.1:5433/fk?sslmode=disable"
 
-export FIELDKIT_INFLUX_DB_TOKEN="RxdlPKjlsec_NdsTabjga3qNxBU0nAAGEuUMSZVcWX-_R3VXi1YTAtku5fZ5cO8LcbooNIh6qrmjrZRZNqVAOQ=="
-export FIELDKIT_INFLUX_DB_URL="http://127.0.0.1:8086"
-export FIELDKIT_INFLUX_DB_ORG="fk"
-export FIELDKIT_INFLUX_DB_BUCKET="sensors"
+# export FIELDKIT_INFLUX_DB_TOKEN="RxdlPKjlsec_NdsTabjga3qNxBU0nAAGEuUMSZVcWX-_R3VXi1YTAtku5fZ5cO8LcbooNIh6qrmjrZRZNqVAOQ=="
+# export FIELDKIT_INFLUX_DB_URL="http://127.0.0.1:8086"
+# export FIELDKIT_INFLUX_DB_ORG="fk"
+# export FIELDKIT_INFLUX_DB_BUCKET="sensors"
 
 make movedata && time build/movedata "$@"
 
