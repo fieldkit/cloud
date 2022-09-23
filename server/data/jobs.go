5926261e3e08a0ddd64d8b7a1fb3d3064a507bd4
Jacob Lewallen <jlewallen@gmail.com>
2022-09-22 17:37:29 -0700

    fix: Unused station progress call was using old que table.

--- server/data/jobs.go
+++ server/data/jobs.go
@@ -8,7 +8,7 @@ type QueJob struct {
 	Priority  int32     `db:"priority"`
 	RunAt     time.Time `db:"run_at"`
 	JobID     int64     `db:"job_id"`
-	JobClass  string    `db:"job_class"`
+	JobClass  string    `db:"job_type"`
 	Args      string    `db:"args"`
 	Errors    int32     `db:"error_count"`
 	LastError *string   `db:"last_error"`
