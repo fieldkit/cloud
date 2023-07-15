e8a0c3d314adca41f2bc2e911513738be69701f9
Jacob Lewallen <jlewallen@gmail.com>
2023-07-14 17:36:27 -0700

    Start on new exporter that works from fkb.

--- server/backend/export_data_handler.go
+++ server/backend/export_data_handler.go
@@ -2,6 +2,7 @@ package backend
 
 import (
 	"context"
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -10,10 +11,10 @@ import (
 	"github.com/pkg/profile"
 
 	"github.com/fieldkit/cloud/server/common/sqlxcache"
+	pb "github.com/fieldkit/data-protocol"
 
 	"github.com/fieldkit/cloud/server/backend/repositories"
 	"github.com/fieldkit/cloud/server/common/logging"
-	"github.com/fieldkit/cloud/server/data"
 	"github.com/fieldkit/cloud/server/files"
 	"github.com/fieldkit/cloud/server/messages"
 )
@@ -36,34 +37,6 @@ func NewExportDataHandler(db *sqlxcache.DB, files files.FileArchive, metrics *lo
 		metrics:   metrics,
 		updatedAt: time.Now(),
 	}
-}
-
-func (h *ExportDataHandler) createFormat(format string) (ExportFormat, error) {
-	if format == CSVFormatter {
-		return NewCsvFormatter(), nil
-	}
-	if format == JSONLinesFormatter {
-		return NewJSONLinesFormatter(), nil
-	}
-	return nil, fmt.Errorf("unknown format: %v", format)
-}
-
-func (h *ExportDataHandler) updateProgress(ctx context.Context, de *data.DataExport, progress float64, message string) error {
-	elapsed := time.Now().Sub(h.updatedAt)
-	if elapsed.Seconds() < SecondsBetweenProgressUpdates {
-		return nil
-	}
-	h.updatedAt = time.Now()
-	de.Progress = progress
-	de.Message = &message
-	r, err := repositories.NewExportRepository(h.db)
-	if err != nil {
-		return err
-	}
-	if _, err := r.UpdateDataExport(ctx, de); err != nil {
-		return err
-	}
-	return nil
 }
 
 func (h *ExportDataHandler) Handle(ctx context.Context, m *messages.ExportData) error {
@@ -95,72 +68,81 @@ func (h *ExportDataHandler) Handle(ctx context.Context, m *messages.ExportData) 
 		return fmt.Errorf("invalid query params: %w", err)
 	}
 
-	format, err := h.createFormat(m.Format)
-	if err != nil {
-		return err
-	}
-
 	log.Infow("parameters", "start", qp.Start, "end", qp.End, "sensors", qp.Sensors, "stations", qp.Stations)
 
-	exporter, err := NewExporter(h.db)
+	ir := repositories.NewIngestionRepository(h.db)
+
+	ingestions, err := ir.QueryByStationID(ctx, qp.Stations[0])
 	if err != nil {
 		return err
 	}
 
-	readFunc := func(ctx context.Context, reader io.Reader) error {
-		log.Infow("archiver:ready")
+	exporter := NewFkbExporter(h.files, h.metrics, nil)
 
-		metadata := make(map[string]string)
-		contentType := format.MimeType()
-		af, err := h.files.Archive(ctx, contentType, metadata, reader)
-		if err != nil {
-			log.Errorw("archiver:error", "error", err)
-			return err
-		} else {
-			log.Infow("archiver:done", "key", af.Key, "bytes", af.BytesRead)
+	for _, ingestion := range ingestions {
+		if err := exporter.Prepare(ctx, ingestion.URL); err != nil {
+			return fmt.Errorf("pass 1: exporting %v failed: %w", ingestion.URL, err)
 		}
-
-		now := time.Now()
-		size := int32(af.BytesRead)
-
-		de.DownloadURL = &af.URL
-		de.CompletedAt = &now
-		de.Progress = 100
-		de.Size = &size
-		if _, err := r.UpdateDataExport(ctx, de); err != nil {
-			return err
-		}
-		return nil
 	}
 
-	progressFunc := func(ctx context.Context, progress float64, message string) error {
-		return h.updateProgress(ctx, de, progress, message)
+	for _, ingestion := range ingestions {
+		if err := exporter.Export(ctx, ingestion.URL); err != nil {
+			return fmt.Errorf("pass 2: exporting %v failed: %w", ingestion.URL, err)
+		}
 	}
 
-	writeFunc := func(ctx context.Context, writer io.Writer) error {
-		criteria := &ExportCriteria{
-			Start:    qp.Start,
-			End:      qp.End,
-			Stations: qp.Stations,
-			Sensors:  qp.Sensors,
-		}
+	return nil
+}
 
-		err = exporter.Export(ctx, criteria, format, progressFunc, writer)
-		if err != nil {
-			return err
-		}
+type FkbExporter struct {
+	writer *csv.Writer
+	walker *FkbWalker
+}
 
-		return nil
+func NewFkbExporter(files files.FileArchive, metrics *logging.Metrics, writer io.Writer) (self *FkbExporter) {
+	self = &FkbExporter{
+		writer: csv.NewWriter(writer),
 	}
+	self.walker = NewFkbWalker(files, metrics, self, true)
+	return
+}
 
-	async := NewAsyncFileWriter(readFunc, writeFunc)
-	if err := async.Start(ctx); err != nil {
+func (e *FkbExporter) Prepare(ctx context.Context, url string) error {
+	if _, err := e.walker.WalkUrl(ctx, url); err != nil {
 		return err
 	}
 
-	if err := async.Wait(ctx); err != nil {
+	return nil
+}
+
+func (e *FkbExporter) Export(ctx context.Context, url string) error {
+	if _, err := e.walker.WalkUrl(ctx, url); err != nil {
 		return err
 	}
 
+	return nil
+}
+
+func (e *FkbExporter) OnSignedMeta(ctx context.Context, signedRecord *pb.SignedRecord, rawRecord *pb.DataRecord, bytes []byte) error {
+	log := Logger(ctx).Sugar()
+
+	log.Infow("signed-meta", "record_number", signedRecord.Record, "record", rawRecord)
+
+	return nil
+}
+
+func (e *FkbExporter) OnMeta(ctx context.Context, recordNumber int64, rawRecord *pb.DataRecord, bytes []byte) error {
+	log := Logger(ctx).Sugar()
+
+	log.Infow("meta", "record_number", recordNumber, "record", rawRecord)
+
+	return nil
+}
+
+func (e *FkbExporter) OnData(ctx context.Context, rawRecord *pb.DataRecord, rawMetaUnused *pb.DataRecord, bytes []byte) error {
+	return nil
+}
+
+func (e *FkbExporter) OnDone(ctx context.Context) (err error) {
 	return nil
 }
